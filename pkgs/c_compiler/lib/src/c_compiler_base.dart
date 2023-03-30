df3c5d85a3d439869e23c4b0964253bb320e6c70
Daco Harkes <dacoharkes@google.com>
2023-03-30 10:33:22 +0000

    Fix hints/warnings

--- pkgs/c_compiler/lib/src/c_compiler_base.dart
+++ pkgs/c_compiler/lib/src/c_compiler_base.dart
@@ -1,5 +1,3 @@
-// TODO: Put public facing types in this file.
-
 /// Checks if you are awesome. Spoiler: you are.
 class Awesome {
   bool get isAwesome => true;
--- pkgs/native_assets_cli/lib/native_assets_cli.dart
+++ pkgs/native_assets_cli/lib/native_assets_cli.dart
@@ -1,4 +1,5 @@
-/// A library that contains the argument and file formats for implementing a native assets CLI.
+/// A library that contains the argument and file formats for implementing a
+/// native assets CLI.
 library;
 
 export 'src/native_assets_cli_base.dart';
