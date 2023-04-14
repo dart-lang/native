3907fc86ddb5a65b456ba346783783634bae7707
Daco Harkes <dacoharkes@google.com>
2023-04-14 09:50:59 +0000

    Fix imports

--- pkgs/c_compiler/test/cbuilder/cbuilder_cross_linux_host_test.dart
+++ pkgs/c_compiler/test/cbuilder/cbuilder_cross_linux_host_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:c_compiler/c_compiler.dart';
 import 'package:c_compiler/src/utils/run_process.dart';
 import 'package:native_assets_cli/native_assets_cli.dart';
