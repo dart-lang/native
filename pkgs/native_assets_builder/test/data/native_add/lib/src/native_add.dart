90ad8554b0fabeada3bd16aba2096aaaa1be9f7f
Daco Harkes <dacoharkes@google.com>
2023-07-04 22:05:53 +0200

    Remove old todos

--- pkgs/native_assets_builder/test/data/native_add/lib/src/native_add.dart
+++ pkgs/native_assets_builder/test/data/native_add/lib/src/native_add.dart
@@ -4,6 +4,4 @@
 
 import 'native_add_bindings_generated.dart' as bindings;
 
-// TODO(dacoharkes): This will fail lookup until the Dart SDK consumes the
-// output of `build.dart`.
 int add(int a, int b) => bindings.add(a, b);
