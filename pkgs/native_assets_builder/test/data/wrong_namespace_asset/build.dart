2598ac66148f5b6a44d8f6bf01643c6b1c49e590
Daco Harkes <dc.harkes@gmail.com>
2023-07-20 07:47:32 +0200

    Package name spacing (#101)

--- pkgs/native_assets_builder/test/data/wrong_namespace_asset/build.dart
+++ pkgs/native_assets_builder/test/data/wrong_namespace_asset/build.dart
@@ -7,10 +7,18 @@ import 'package:native_assets_cli/native_assets_cli.dart';
 void main(List<String> args) async {
   final buildConfig = await BuildConfig.fromArgs(args);
   final buildOutput = BuildOutput(
-    metadata: Metadata({
-      'some_key': 'some_value',
-      'some_int': 3,
-    }),
+    assets: [
+      Asset(
+        name: 'package:other_package/foo',
+        linkMode: LinkMode.dynamic,
+        target: Target.current,
+        path: AssetAbsolutePath(
+          buildConfig.outDir.resolve(
+            Target.current.os.dylibFileName('foo'),
+          ),
+        ),
+      ),
+    ],
   );
   await buildOutput.writeToFile(outDir: buildConfig.outDir);
 }
