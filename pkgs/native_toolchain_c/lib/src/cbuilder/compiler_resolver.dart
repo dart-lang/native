18bff8ce73ac758a79fe5b8290208559862d35d4
Ryan Macnak <rmacnak@google.com>
2023-10-25 19:18:01 -0700

    [native_toolchain_c] Setup Linux X64 toolchain for non-X64 hosts. (#164)

--- pkgs/native_toolchain_c/lib/src/cbuilder/compiler_resolver.dart
+++ pkgs/native_toolchain_c/lib/src/cbuilder/compiler_resolver.dart
@@ -74,6 +74,8 @@ class CompilerResolver {
           return aarch64LinuxGnuGcc;
         case Architecture.ia32:
           return i686LinuxGnuGcc;
+        case Architecture.x64:
+          return x86_64LinuxGnuGcc;
         case Architecture.riscv64:
           return riscv64LinuxGnuGcc;
       }
@@ -160,6 +162,8 @@ class CompilerResolver {
           return aarch64LinuxGnuGccAr;
         case Architecture.ia32:
           return i686LinuxGnuGccAr;
+        case Architecture.x64:
+          return x86_64LinuxGnuGccAr;
         case Architecture.riscv64:
           return riscv64LinuxGnuGccAr;
       }
