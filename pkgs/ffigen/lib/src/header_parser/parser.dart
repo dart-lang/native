eb1637c1f8f92f21b138c683854ec526fd62b8f4
Prerak Mann <mannprerak2@gmail.com>
2023-11-19 23:39:00 +0530

    temp

--- pkgs/ffigen/lib/src/header_parser/parser.dart
+++ pkgs/ffigen/lib/src/header_parser/parser.dart
@@ -124,7 +124,9 @@ List<Binding> parseToBindings(Config c) {
     } else {
       _logger.severe(
           "Skipped generating bindings due to errors in source files. Either resolve or ignore them (Set --ignore-source-errors on cmd or ignore-source-errors:true in config.");
-      exit(1);
+      // TEMP: REVERT THIS CHANGE
+      _logger.severe(config.headers.entryPoints);
+      // exit(1);
     }
   }
 
