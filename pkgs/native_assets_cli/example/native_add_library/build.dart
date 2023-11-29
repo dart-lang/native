ded78fffd4f8674ec090dfbf4556c86eeca7505f
Moritz <mosum@google.com>
2023-11-29 13:46:00 +0100

    Fix path

--- pkgs/native_assets_cli/example/native_add_library/build.dart
+++ pkgs/native_assets_cli/example/native_add_library/build.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:logging/logging.dart';
 import 'package:native_assets_cli/native_assets_cli.dart';
 import 'package:native_toolchain_c/native_toolchain_c.dart';
@@ -38,8 +40,10 @@ void main(List<String> args) async {
       id: 'package:$packageName/ajsonfile',
       linkMode: LinkMode.dynamic,
       target: buildConfig.target,
-      path: AssetAbsolutePath(Uri.file(
-          '/home/mosum/projects/native/pkgs/native_assets_cli/example/native_add_library/data_asset_build.json')),
+      path: AssetAbsolutePath(File(
+              'native/pkgs/native_assets_cli/example/native_add_library/data_asset_build.json')
+          .absolute
+          .uri),
       type: AssetType.data,
     ),
   );
