24393296cc31bef8ac8a825c83654cf96b956af2
Daco Harkes <dacoharkes@google.com>
2024-01-09 12:50:23 +0100

    Fix analysis against published dependency

--- pkgs/native_assets_builder/test/model/asset_test.dart
+++ pkgs/native_assets_builder/test/model/asset_test.dart
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: undefined_hidden_name
+
 import 'package:native_assets_builder/src/model/asset.dart';
-import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:native_assets_cli/native_assets_cli.dart'
+    hide AssetIterable, AssetRelativePath;
 import 'package:test/test.dart';
 
 void main() {
