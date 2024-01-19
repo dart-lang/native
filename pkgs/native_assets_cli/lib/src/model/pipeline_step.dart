94a5d42383b1486de6e383cd0fa34c8770a0df18
Moritz <mosum@google.com>
2024-01-19 16:03:31 +0100

    Add breaking flag to CI

--- pkgs/native_assets_cli/lib/src/model/pipeline_step.dart
+++ pkgs/native_assets_cli/lib/src/model/pipeline_step.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 /// The two types of scripts which are hooked into the compilation process.
 ///
 /// The `build.dart` script runs before, and the `link.dart` script after
