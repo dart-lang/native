a5b98cfef13cd7b89fa1e7e22e2e23db0fbf5979
Moritz <mosum@google.com>
2024-01-19 10:48:57 +0100

    Remove step from assets

--- pkgs/native_assets_cli/lib/src/model/asset.dart
+++ pkgs/native_assets_cli/lib/src/model/asset.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:collection/collection.dart';
 import 'package:yaml/yaml.dart';
 
 import '../api/asset.dart' as api;
@@ -139,20 +138,11 @@ class Asset implements api.Asset {
   @override
   final AssetPath path;
 
-  /// The step at which the asset should be written to file.
-  /// * [PipelineStep.build] - The asset is written after `build.dart` has run.
-  /// * [PipelineStep.build] - The asset is written after `link.dart` has run.
-  ///
-  /// In particular, an asset which is created by `build.dart` and then further
-  /// modified by `link.dart` should be marked as [PipelineStep.link].
-  final PipelineStep step;
-
   Asset({
     required this.id,
     required this.linkMode,
     required this.target,
     required this.path,
-    this.step = PipelineStep.build,
   });
 
   factory Asset.fromYaml(YamlMap yamlMap) => Asset(
@@ -160,9 +150,6 @@ class Asset implements api.Asset {
         path: AssetPath.fromYaml(as<YamlMap>(yamlMap[_pathKey])),
         target: Target.fromString(as<String>(yamlMap[_targetKey])),
         linkMode: LinkMode.fromName(as<String>(yamlMap[_linkModeKey])),
-        step: PipelineStep.values.firstWhereOrNull(
-                (step) => step.name == as<String>(yamlMap[_step])) ??
-            PipelineStep.build,
       );
 
   static List<Asset> listFromYamlString(String yaml) {
@@ -193,7 +180,6 @@ class Asset implements api.Asset {
         linkMode: linkMode ?? this.linkMode,
         target: target ?? this.target,
         path: path ?? this.path,
-        step: step ?? this.step,
       );
 
   @override
@@ -204,26 +190,23 @@ class Asset implements api.Asset {
     return other.id == id &&
         other.linkMode == linkMode &&
         other.target == target &&
-        other.path == path &&
-        other.step == step;
+        other.path == path;
   }
 
   @override
-  int get hashCode => Object.hash(id, linkMode, target, path, step);
+  int get hashCode => Object.hash(id, linkMode, target, path);
 
   Map<String, Object> toYaml() => {
         _idKey: id,
         _linkModeKey: linkMode.name,
         _pathKey: path.toYaml(),
         _targetKey: target.toString(),
-        _step: step.name,
       };
 
   static const _idKey = 'id';
   static const _linkModeKey = 'link_mode';
   static const _pathKey = 'path';
   static const _targetKey = 'target';
-  static const _step = 'step';
 
   // Future<bool> exists() => path.exists();
 
