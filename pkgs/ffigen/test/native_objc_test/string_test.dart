9fe667df781c6270caf7bc7228751356a838ec1d
Liam Appelbe <liama@google.com>
2024-04-17 16:07:53 +1200

    Remove coverage debugging

--- pkgs/ffigen/test/native_objc_test/string_test.dart
+++ pkgs/ffigen/test/native_objc_test/string_test.dart
@@ -8,11 +8,11 @@
 import 'dart:ffi';
 import 'dart:io';
 
-// import 'package:objective_c/objective_c.dart';
+import 'package:objective_c/objective_c.dart';
 import 'package:test/test.dart';
 
 import '../test_utils.dart';
-// import 'string_bindings.dart';
+import 'string_bindings.dart';
 import 'util.dart';
 
 void main() {
@@ -25,27 +25,27 @@ void main() {
       generateBindingsForCoverage('string');
     });
 
-    // for (final s in ['Hello', '🇵🇬', 'Embedded\u0000Null']) {
-    //   test('NSString to/from Dart string [$s]', () {
-    //     final ns1 = NSString(s);
-    //     expect(ns1.length, s.length);
-    //     expect(ns1.toString().length, s.length);
-    //     expect(ns1.toString(), s);
+    for (final s in ['Hello', '🇵🇬', 'Embedded\u0000Null']) {
+      test('NSString to/from Dart string [$s]', () {
+        final ns1 = NSString(s);
+        expect(ns1.length, s.length);
+        expect(ns1.toString().length, s.length);
+        expect(ns1.toString(), s);
 
-    //     final ns2 = s.toNSString();
-    //     expect(ns2.length, s.length);
-    //     expect(ns2.toString().length, s.length);
-    //     expect(ns2.toString(), s);
-    //   });
-    // }
+        final ns2 = s.toNSString();
+        expect(ns2.length, s.length);
+        expect(ns2.toString().length, s.length);
+        expect(ns2.toString(), s);
+      });
+    }
 
-    // test('strings usable', () {
-    //   final str1 = 'Hello'.toNSString();
-    //   final str2 = 'World!'.toNSString();
+    test('strings usable', () {
+      final str1 = 'Hello'.toNSString();
+      final str2 = 'World!'.toNSString();
 
-    //   final str3 = StringUtil.strConcat_with_(str1, str2);
-    //   expect(str3.length, 11);
-    //   expect(str3.toString(), "HelloWorld!");
-    // });
+      final str3 = StringUtil.strConcat_with_(str1, str2);
+      expect(str3.length, 11);
+      expect(str3.toString(), "HelloWorld!");
+    });
   });
 }
