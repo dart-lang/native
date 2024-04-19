61e70dc85d8072ff2d2e37169ace459cae3e59c1
Moritz <mosum@google.com>
2024-04-19 11:02:12 +0200

    Fix analyze issues

--- pkgs/native_assets_cli/lib/src/api/build_config.dart
+++ pkgs/native_assets_cli/lib/src/api/build_config.dart
@@ -2,12 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:cli_config/cli_config.dart';
 import 'package:collection/collection.dart';
-import 'package:crypto/crypto.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 import '../../native_assets_cli_internal.dart';
