282f7f12fc6e1a236c47327d8b989e60ccc97f62
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-05-14 19:12:18 +0300

    Added license comments

--- pkgs/swiftgen/swift2objc/Package.swift
+++ pkgs/swiftgen/swift2objc/Package.swift
@@ -1,6 +1,10 @@
 // swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import PackageDescription
 
 let package = Package(
