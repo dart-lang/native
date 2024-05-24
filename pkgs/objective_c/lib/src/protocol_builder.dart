0f3e08b583edfeb444446f7c00249f723827ab63
Liam Appelbe <liama@google.com>
2024-05-24 15:41:53 +1200

    Fix analysis

--- pkgs/objective_c/lib/src/protocol_builder.dart
+++ pkgs/objective_c/lib/src/protocol_builder.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'c_bindings_generated.dart' as c;
 import 'objective_c_bindings_generated.dart' as objc;
 import 'internal.dart' show ObjCBlockBase, ObjCProtocolMethod;
 
