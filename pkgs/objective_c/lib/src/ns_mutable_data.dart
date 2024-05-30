06f3c22f3f199b4cd0f13315ef3680d756f71c79
Brian Quinlan <bquinlan@google.com>
2024-05-30 14:02:34 -0700

    Update ns_mutable_data.dart

--- pkgs/objective_c/lib/src/ns_mutable_data.dart
+++ pkgs/objective_c/lib/src/ns_mutable_data.dart
@@ -2,9 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:collection';
 import 'dart:ffi';
-import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
 
