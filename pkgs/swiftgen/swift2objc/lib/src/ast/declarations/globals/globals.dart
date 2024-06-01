73723be9f38bbb48614d4928dde2778e0ed35277
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-06-01 10:38:33 +0300

    added license comments

--- pkgs/swiftgen/swift2objc/lib/src/ast/declarations/globals/globals.dart
+++ pkgs/swiftgen/swift2objc/lib/src/ast/declarations/globals/globals.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import '../../_core/interfaces/declaration.dart';
 import '../../_core/interfaces/genericable.dart';
 import '../../_core/interfaces/paramable.dart';
