8c70f75f527c85ad49a3c4e5287c62c54d2447c1
Levi Lesches <levi.lesches@kdab.com>
2024-06-07 18:26:00 -0400

    Native enums are now Dart enums instead of classes

--- pkgs/ffigen/lib/src/code_generator/enum_class.dart
+++ pkgs/ffigen/lib/src/code_generator/enum_class.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:collection/collection.dart';
+
 import 'binding.dart';
 import 'binding_string.dart';
 import 'native_type.dart';
@@ -9,18 +11,35 @@ import 'type.dart';
 import 'utils.dart';
 import 'writer.dart';
 
+// Only uniques
+// duplicate --> original
+// unique --> all duplicates
+// typedef UniqueAndDuplicates = ({EnumConstant unique, List<EnumConstant> duplicates});
+// typedef DuplicateAndOriginal = ({EnumConstant duplicate, })
+
 /// A binding for enums in C.
 ///
 /// For a C enum -
 /// ```c
-/// enum Fruits {apple, banana = 10};
+/// enum Fruits {apple, banana = 10, yellow_fruit = 10};
 /// ```
 /// The generated dart code is
 ///
 /// ```dart
-/// class Fruits {
-///   static const apple = 0;
-///   static const banana = 10;
+/// enum Fruits {
+///   apple(0),
+///   banana(10);
+/// 
+///   static const yellow_fruit = banana;
+/// 
+///   final int value;
+///   const Fruit(this.value);
+/// 
+///   @override
+///   String toString() {
+///     if (this == banana) return "Fruits.banana, Fruits.yellow_fruit";
+///     return super.toString();
+///   }
 /// }
 /// ```
 class EnumClass extends BindingType {
@@ -28,43 +47,140 @@ class EnumClass extends BindingType {
 
   final List<EnumConstant> enumConstants;
 
+  /// Avoids naming members with [name] because dart doesn't allow values to
+  /// have the same name as the class.
+  final UniqueNamer namer;
+
+  final Map<EnumConstant, String> enumNames = {};
+
+  static const depth = '  ';
+
+  final Map<EnumConstant, List<EnumConstant>> uniqueToDuplicates = {};
+  final Map<EnumConstant, EnumConstant> duplicateToOriginal = {};
+  final Set<EnumConstant> uniqueMembers = {};
+
+  String formatValue(EnumConstant ec) {
+    final buffer = StringBuffer();
+    final enumValueName = namer.makeUnique(ec.name);
+    enumNames[ec] = enumValueName;
+    if (ec.dartDoc != null) {
+      buffer.write('$depth/// ');
+      buffer.writeAll(ec.dartDoc!.split('\n'), '\n$depth/// ');
+      buffer.write('\n');
+    }
+    buffer.write('$depth$enumValueName(${ec.value})');
+    return buffer.toString();
+  }
+
+  void scanForDuplicates() {
+    uniqueMembers.clear();
+    uniqueToDuplicates.clear();
+    duplicateToOriginal.clear();
+    for (final ec in enumConstants) {
+      final original = uniqueMembers.firstWhereOrNull((other) => other.value == ec.value);
+      if (original == null) {
+        // This is a unique entry
+        uniqueMembers.add(ec);
+        uniqueToDuplicates[ec] = [];
+      } else {
+        // This is a duplicate of a previous entry
+        duplicateToOriginal[ec] = original;
+        uniqueToDuplicates[original]!.add(ec);
+      }
+    }
+  }
+
   EnumClass({
     super.usr,
     super.originalName,
     required super.name,
     super.dartDoc,
     List<EnumConstant>? enumConstants,
-  }) : enumConstants = enumConstants ?? [];
+  }) : 
+    enumConstants = enumConstants ?? [],
+    namer = UniqueNamer({name})
+  { 
+    scanForDuplicates();
+  }
+
+  void writeUniqueMembers(StringBuffer s) {
+    s.write("$depth// ===== Unique members =====\n");
+    s.writeAll(uniqueMembers.map(formatValue), ",\n");
+    if (uniqueMembers.isNotEmpty) s.write('$depth;\n\n');
+  }
+
+  void writeDuplicateMembers(StringBuffer s) {
+    s.write("$depth// ===== Aliases =====\n");
+    for (final entry in duplicateToOriginal.entries) {
+      final duplicate = entry.key;
+      final original = entry.value;
+      final duplicateName = namer.makeUnique(duplicate.name);
+      enumNames[duplicate] = duplicateName;
+      // [!] Each original enum value was given a name in [writeUniqueMembers].
+      final originalName = enumNames[original]!;
+      s.write("${depth}static const $duplicateName = $originalName;\n");
+    }
+    s.write("\n");
+  }
+
+  void writeConstructor(StringBuffer s) {
+    s.write("$depth// ===== Constructor =====\n");
+    s.write("${depth}final int value;\n");
+    s.write("${depth}const $name(this.value);\n\n");
+  }
+
+  void writeToStringOverride(StringBuffer s) {
+    s.write("$depth// ===== Override toString() for aliases =====\n");
+    s.write("$depth@override\n");
+    s.write("${depth}String toString() {\n");
+    for (final entry in uniqueToDuplicates.entries) {
+      // [!] All enum values were given a name when their declarations were generated
+      final unique = entry.key;
+      final originalName = enumNames[unique]!;      
+      final duplicates = entry.value;
+      if (duplicates.isEmpty) continue;
+      final allDuplicates = [
+        for (final duplicate in [unique] + duplicates)
+          "$name.${enumNames[duplicate]!}",
+      ].join(", ");
+      s.write('${depth * 2}if (this == $originalName) return "$allDuplicates";\n');
+    }
+    s.write("${depth * 2}return super.toString();\n");
+    s.write("$depth}\n");
+  }
+
+  void writeDartDoc(StringBuffer s) {
+    if (dartDoc != null) {
+      s.write(makeDartDoc(dartDoc!));
+    }
+  }
+
+  void writeEmptyEnum(StringBuffer s) {
+    s.write("sealed class $name { }\n");
+  }
 
   @override
   BindingString toBindingString(Writer w) {
     final s = StringBuffer();
-    final enclosingClassName = name;
+    scanForDuplicates();
 
-    if (dartDoc != null) {
-      s.write(makeDartDoc(dartDoc!));
+    // Print the dartdoc.
+    writeDartDoc(s);
+    if (enumConstants.isEmpty) {
+      writeEmptyEnum(s);
+    } else {
+      s.write('enum $name {\n');
+        writeUniqueMembers(s);
+        writeDuplicateMembers(s);
+        writeConstructor(s);
+        writeToStringOverride(s);
+      s.write('}\n\n');
     }
-
-    /// Adding [enclosingClassName] because dart doesn't allow class member
-    /// to have the same name as the class.
-    final localUniqueNamer = UniqueNamer({enclosingClassName});
-
-    // Print enclosing class.
-    s.write('abstract class $enclosingClassName {\n');
-    const depth = '  ';
-    for (final ec in enumConstants) {
-      final enumValueName = localUniqueNamer.makeUnique(ec.name);
-      if (ec.dartDoc != null) {
-        s.write('$depth/// ');
-        s.writeAll(ec.dartDoc!.split('\n'), '\n$depth/// ');
-        s.write('\n');
-      }
-      s.write('${depth}static const int $enumValueName = ${ec.value};\n');
-    }
-    s.write('}\n\n');
 
     return BindingString(
-        type: BindingStringType.enumClass, string: s.toString());
+      type: BindingStringType.enum_, 
+      string: s.toString(),
+    );
   }
 
   @override
