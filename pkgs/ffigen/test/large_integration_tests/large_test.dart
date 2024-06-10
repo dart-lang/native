f0d7f96bbd766cbf996e546f078a307d2cccf6b0
Levi Lesches <levi.lesches@kdab.com>
2024-06-09 21:50:05 -0400

    Removed print statement

--- pkgs/ffigen/test/large_integration_tests/large_test.dart
+++ pkgs/ffigen/test/large_integration_tests/large_test.dart
@@ -44,7 +44,6 @@ ${strings.typeMap}:
 ${strings.preamble}: |
   // ignore_for_file: camel_case_types, non_constant_identifier_names
       ''');
-      print(config.headers.entryPoints);
       final library = parse(config);
 
       matchLibraryWithExpected(
