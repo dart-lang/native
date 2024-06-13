241585d11c448987d22937eedcb28bad93d39ccf
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-06-13 13:42:09 +0300

    add return type for parseSymbolName funciton

--- pkgs/swiftgen/swift2objc/lib/src/parser/_core/utils/parse_symbol_name.dart
+++ pkgs/swiftgen/swift2objc/lib/src/parser/_core/utils/parse_symbol_name.dart
@@ -1,6 +1,6 @@
 import '../type_defs.dart';
 
-parseSymbolName(JsonMap symbolJson) {
+String parseSymbolName(JsonMap symbolJson) {
   final List subHeadings = symbolJson["names"]["subHeading"];
   return subHeadings.firstWhere(
     (map) => map["kind"] == "identifier",
