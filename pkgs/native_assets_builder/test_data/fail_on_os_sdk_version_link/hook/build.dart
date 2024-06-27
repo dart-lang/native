f26c77e2b73ab7a9cbef1dd6b12d0692a9802b66
Daco Harkes <dacoharkes@google.com>
2024-06-27 14:28:30 +0200

    more analysis

--- pkgs/native_assets_builder/test_data/fail_on_os_sdk_version_link/hook/build.dart
+++ pkgs/native_assets_builder/test_data/fail_on_os_sdk_version_link/hook/build.dart
@@ -4,22 +4,6 @@
 
 import 'package:native_assets_cli/native_assets_cli.dart';
 
-// From https://docs.flutter.dev/reference/supported-platforms.
-const flutterAndroidNdkVersionLowestBestEffort = 19;
-const flutterAndroidNdkVersionLowestSupported = 21;
-const flutterAndroidNdkVersionHighestSupported = 34;
-const flutteriOSLowestBestEffort = 12;
-const flutteriOSLowestSupported = 17;
-const flutteriOSHighestSupported = 17;
-const flutterMacOSLowestBestEffort = 12;
-const flutterMacOSLowestSupported = 13;
-const flutterMacOSHighestSupported = 13;
-
-// Simulate needing some version of the API.
-const minNdkApiVersionForThisPackage = 28;
-const minIosVersionForThisPackage = 16;
-const minMacOSVersionForThisPackage = 13;
-
 void main(List<String> arguments) async {
   await build(arguments, (config, output) async {
     output.addAsset(
