1d2a9af99b809911ae0f9276b9705c543e94f734
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2024-07-06 06:38:54 +0300

    edit source.swift

--- pkgs/swiftgen/swift2objc/lib/src/temp_test/symbolgraph/source.swift
+++ pkgs/swiftgen/swift2objc/lib/src/temp_test/symbolgraph/source.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public class MyClass {
-    public func myMethod(label1 param1: Int, param2: MyOtherClass) -> Int {
+    public func myMethod(label1 param1: Int, param2: MyOtherClass) -> MyOtherClass {
         return MyOtherClass()
     }
 }
