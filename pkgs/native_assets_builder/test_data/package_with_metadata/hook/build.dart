82887c2479b3cf115d6cc651908a9c9dd2ccf52e
Daco Harkes <dacoharkes@google.com>
2024-07-15 14:40:05 +0200

    [native_assets_cli] Deprecate metadata (#1341)
    We might not address this issue fully before stable.
    
    * https://github.com/dart-lang/native/issues/1251
    
    But we're quite sure to remove the current metadata API, so deprecate it.

--- pkgs/native_assets_builder/test_data/package_with_metadata/hook/build.dart
+++ pkgs/native_assets_builder/test_data/package_with_metadata/hook/build.dart
@@ -6,6 +6,7 @@ import 'package:native_assets_cli/native_assets_cli.dart';
 
 void main(List<String> arguments) async {
   await build(arguments, (config, output) async {
+    // ignore: deprecated_member_use
     output.addMetadata({
       'some_key': 'some_value',
       'some_int': 3,
