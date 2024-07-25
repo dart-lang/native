67b39c8a52c88d138efc27c0e53fbbd37c863d9c
TheComputerM <mudit.somani00@gmail.com>
2024-07-25 12:07:22 +0530

    [ffigenpad] init project

---
+++ pkgs/ffigenpad/bin/ffigenpad.dart
@@ -0,0 +1,5 @@
+import 'package:ffigenpad/ffigenpad.dart' as ffigenpad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${ffigenpad.calculate()}!');
+}
---
+++ pkgs/ffigenpad/test/ffigenpad_test.dart
@@ -0,0 +1,8 @@
+import 'package:ffigenpad/ffigenpad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
