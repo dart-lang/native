84215accfe0a4664468d53b1b3dd40a3039018eb
Moritz <mosum@google.com>
2024-07-25 14:09:36 +0200

    Fix ordering

--- pkgs/native_assets_builder/test_data/treeshaking_native_libs/test/treeshaking_native_libs_test.dart
+++ pkgs/native_assets_builder/test_data/treeshaking_native_libs/test/treeshaking_native_libs_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:treeshaking_native_libs/treeshaking_native_libs.dart';
 import 'package:test/test.dart';
+import 'package:treeshaking_native_libs/treeshaking_native_libs.dart';
 
 void main() {
   test('native add test', () {
