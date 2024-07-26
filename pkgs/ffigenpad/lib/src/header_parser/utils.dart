46e3e235ddf665acacf3b834952ed7b94d9966b3
TheComputerM <mudit.somani00@gmail.com>
2024-07-26 13:48:09 +0530

    [ffigenpad] port some utils

--- pkgs/ffigenpad/lib/src/header_parser/utils.dart
+++ pkgs/ffigenpad/lib/src/header_parser/utils.dart
@@ -2,25 +2,52 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "dart:ffi" as ffi;
+import 'dart:ffi';
 import 'package:ffigenpad/src/header_parser/malloc.dart';
-
-import 'clang_bindings/clang_bindings.dart' as clang_types;
+import 'clang_bindings/clang_bindings.dart' as clang;
 import 'dart:convert' as convert;
 import 'dart:js_interop';
+
+export 'package:ffigen/src/header_parser/utils.dart'
+    show Stack, IncrementalNamer, Macro;
 
 /// dart interop for emscripten's addFunction
 @JS()
 external int addFunction(JSExportedDartFunction func, String signature);
 
-extension CXStringExt on ffi.Pointer<clang_types.CXString> {
+extension CXSourceRangeExt on Pointer<clang.CXSourceRange> {
   void dispose() {
-    clang_types.clang_disposeString_wrap(this);
+    malloc.free(this);
+  }
+}
+
+extension CXCursorExt on Pointer<clang.CXCursor> {
+  /// Returns the kind int from [clang.CXCursorKind].
+  int kind() {
+    return clang.clang_getCursorKind_wrap(this).index;
+  }
+
+  /// Name of the cursor (E.g function name, Struct name, Parameter name).
+  String spelling() {
+    return clang.clang_getCursorSpelling_wrap(this).toStringAndDispose();
+  }
+
+  /// Spelling for a [clang.CXCursorKind], useful for debug purposes.
+  String kindSpelling() {
+    return clang
+        .clang_getCursorKindSpelling_wrap(clang.clang_getCursorKind_wrap(this))
+        .toStringAndDispose();
+  }
+}
+
+extension CXStringExt on Pointer<clang.CXString> {
+  void dispose() {
+    clang.clang_disposeString_wrap(this);
   }
 
   /// Converts CXString to dart string and disposes CXString.
   String toStringAndDispose() {
-    final codeUnits = clang_types.clang_getCString_wrap(this);
+    final codeUnits = clang.clang_getCString_wrap(this);
     List<int> output = [];
     int length = 0;
     while (codeUnits[length] != 0) {
@@ -32,15 +59,43 @@ extension CXStringExt on ffi.Pointer<clang_types.CXString> {
   }
 }
 
+extension CXTypeExt on Pointer<clang.CXType> {
+  String spelling() {
+    return clang.clang_getTypeSpelling_wrap(this).toStringAndDispose();
+  }
+}
+
 extension StringUtf8Pointer on String {
   /// Converts string into an array of bytes and allocates it in WASM memory
-  ffi.Pointer<ffi.Uint8> toNativeUint8() {
+  Pointer<Uint8> toNativeUint8() {
     final units = convert.utf8.encode(this);
-    final result = malloc<ffi.Uint8>(units.length + 1);
+    final result = malloc<Uint8>(units.length + 1);
     for (int i = 0; i < units.length; i++) {
       result[i] = units[i];
     }
     result[units.length] = 0;
     return result;
+  }
+}
+
+/// Converts a [List<String>] to [Pointer<Pointer<Uint8>>].
+Pointer<Pointer<Uint8>> createDynamicStringArray(List<String> list) {
+  final nativeCmdArgs = malloc<Pointer<Uint8>>(list.length);
+
+  for (var i = 0; i < list.length; i++) {
+    nativeCmdArgs[i] = list[i].toNativeUint8();
+  }
+
+  return nativeCmdArgs;
+}
+
+extension DynamicCStringArray on Pointer<Pointer<Uint8>> {
+  // Properly disposes a Pointer<Pointer<Uint8>, ensure that sure length is
+  // correct.
+  void dispose(int length) {
+    for (var i = 0; i < length; i++) {
+      malloc.free(this[i]);
+    }
+    malloc.free(this);
   }
 }
---
+++ pkgs/ffigenpad/lib/src/strings.dart
@@ -0,0 +1,1 @@
+export 'package:ffigen/src/strings.dart';
