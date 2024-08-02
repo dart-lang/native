75e699467c1f9822d626b26bbfddf056db15317f
TheComputerM <mudit.somani00@gmail.com>
2024-08-03 02:07:43 +0530

    [ffigenpad] add config

---
+++ pkgs/ffigenpad/lib/src/config_provider/config.dart
@@ -0,0 +1,309 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../code_generator.dart';
+import 'config_impl.dart';
+import 'config_types.dart';
+// import 'spec_utils.dart';
+
+/// Provides configurations to other modules.
+abstract interface class Config {
+  /// Input config filename, if any.
+  Uri? get filename;
+
+  /// Output file name.
+  Uri get output;
+
+  /// Output ObjC file name.
+  Uri get outputObjC;
+
+  /// Symbol file config.
+  SymbolFile? get symbolFile;
+
+  /// Language that ffigen is consuming.
+  Language get language;
+
+  /// Path to headers. May not contain globs.
+  List<Uri> get entryPoints;
+
+  /// Whether to include a specific header. This exists in addition to
+  /// [entryPoints] to allow filtering of transitively included headers.
+  bool shouldIncludeHeader(Uri header);
+
+  /// CommandLine Arguments to pass to clang_compiler.
+  List<String> get compilerOpts;
+
+  /// VarArg function handling.
+  Map<String, List<VarArgFunction>> get varArgFunctions;
+
+  /// Declaration filters for Functions.
+  DeclarationFilters get functionDecl;
+
+  /// Declaration filters for Structs.
+  DeclarationFilters get structDecl;
+
+  /// Declaration filters for Unions.
+  DeclarationFilters get unionDecl;
+
+  /// Declaration filters for Enums.
+  DeclarationFilters get enumClassDecl;
+
+  /// Declaration filters for Unnamed enum constants.
+  DeclarationFilters get unnamedEnumConstants;
+
+  /// Declaration filters for Globals.
+  DeclarationFilters get globals;
+
+  /// Declaration filters for Macro constants.
+  DeclarationFilters get macroDecl;
+
+  /// Declaration filters for Typedefs.
+  DeclarationFilters get typedefs;
+
+  /// Declaration filters for Objective C interfaces.
+  DeclarationFilters get objcInterfaces;
+
+  /// Declaration filters for Objective C protocols.
+  DeclarationFilters get objcProtocols;
+
+  /// If enabled, unused typedefs will also be generated.
+  bool get includeUnusedTypedefs;
+
+  /// Undocumented option that changes code generation for package:objective_c.
+  /// The main difference is whether NSObject etc are imported from
+  /// package:objective_c (the default) or code genned like any other class.
+  /// This is necessary because package:objective_c can't import NSObject from
+  /// itself.
+  bool get generateForPackageObjectiveC;
+
+  /// If generated bindings should be sorted alphabetically.
+  bool get sort;
+
+  /// If typedef of supported types(int8_t) should be directly used.
+  bool get useSupportedTypedefs;
+
+  /// Stores all the library imports specified by user including those for ffi
+  /// and pkg_ffi.
+  Map<String, LibraryImport> get libraryImports;
+
+  /// Stores all the symbol file maps name to ImportedType mappings specified by
+  /// user.
+  Map<String, ImportedType> get usrTypeMappings;
+
+  /// Stores typedef name to ImportedType mappings specified by user.
+  Map<String, ImportedType> get typedefTypeMappings;
+
+  /// Stores struct name to ImportedType mappings specified by user.
+  Map<String, ImportedType> get structTypeMappings;
+
+  /// Stores union name to ImportedType mappings specified by user.
+  Map<String, ImportedType> get unionTypeMappings;
+
+  /// Stores native int name to ImportedType mappings specified by user.
+  Map<String, ImportedType> get nativeTypeMappings;
+
+  /// Extracted Doc comment type.
+  CommentType get commentType;
+
+  /// Whether structs that are dependencies should be included.
+  CompoundDependencies get structDependencies;
+
+  /// Whether unions that are dependencies should be included.
+  CompoundDependencies get unionDependencies;
+
+  /// Whether, and how, to override struct packing for the given struct.
+  PackingValue? structPackingOverride(Declaration declaration);
+
+  /// The module that the ObjC interface belongs to.
+  String? interfaceModule(Declaration declaration);
+
+  /// The module that the ObjC protocol belongs to.
+  String? protocolModule(Declaration declaration);
+
+  /// Name of the wrapper class.
+  String get wrapperName;
+
+  /// Doc comment for the wrapper class.
+  String? get wrapperDocComment;
+
+  /// Header of the generated bindings.
+  String? get preamble;
+
+  /// If `Dart_Handle` should be mapped with Handle/Object.
+  bool get useDartHandle;
+
+  /// Whether to silence warning for enum integer type mimicking.
+  bool get silenceEnumWarning;
+
+  /// Whether to expose the function typedef for a given function.
+  bool shouldExposeFunctionTypedef(Declaration declaration);
+
+  /// Whether the given function is a leaf function.
+  bool isLeafFunction(Declaration declaration);
+
+  /// Whether to generate the given enum as a series of int constants, rather
+  /// than a real Dart enum.
+  bool enumShouldBeInt(Declaration declaration);
+
+  /// Whether to generate the given unnamed enum as a series of int constants,
+  /// rather than a real Dart enum.
+  bool unnamedEnumsShouldBeInt(Declaration declaration);
+
+  /// Config options for @Native annotations.
+  FfiNativeConfig get ffiNativeConfig;
+
+  /// Where to ignore compiler warnings/errors in source header files.
+  bool get ignoreSourceErrors;
+
+  /// Whether to format the output file.
+  bool get formatOutput;
+
+  factory Config({
+    Uri? filename,
+    Uri? libclangDylib,
+    required Uri output,
+    Uri? outputObjC,
+    SymbolFile? symbolFile,
+    Language language = Language.c,
+    required List<Uri> entryPoints,
+    bool Function(Uri header)? shouldIncludeHeaderFunc,
+    List<String> compilerOpts = const <String>[],
+    Map<String, List<VarArgFunction>> varArgFunctions =
+        const <String, List<VarArgFunction>>{},
+    DeclarationFilters? functionDecl,
+    DeclarationFilters? structDecl,
+    DeclarationFilters? unionDecl,
+    DeclarationFilters? enumClassDecl,
+    DeclarationFilters? unnamedEnumConstants,
+    DeclarationFilters? globals,
+    DeclarationFilters? macroDecl,
+    DeclarationFilters? typedefs,
+    DeclarationFilters? objcInterfaces,
+    DeclarationFilters? objcProtocols,
+    bool includeUnusedTypedefs = false,
+    bool generateForPackageObjectiveC = false,
+    bool sort = false,
+    bool useSupportedTypedefs = true,
+    List<LibraryImport> libraryImports = const <LibraryImport>[],
+    List<ImportedType> usrTypeMappings = const <ImportedType>[],
+    List<ImportedType> typedefTypeMappings = const <ImportedType>[],
+    List<ImportedType> structTypeMappings = const <ImportedType>[],
+    List<ImportedType> unionTypeMappings = const <ImportedType>[],
+    List<ImportedType> nativeTypeMappings = const <ImportedType>[],
+    CommentType? commentType,
+    CompoundDependencies structDependencies = CompoundDependencies.full,
+    CompoundDependencies unionDependencies = CompoundDependencies.full,
+    PackingValue? Function(Declaration declaration)? structPackingOverrideFunc,
+    String? Function(Declaration declaration)? interfaceModuleFunc,
+    String? Function(Declaration declaration)? protocolModuleFunc,
+    String wrapperName = 'NativeLibrary',
+    String? wrapperDocComment,
+    String? preamble,
+    bool useDartHandle = true,
+    bool silenceEnumWarning = false,
+    bool Function(Declaration declaration)? shouldExposeFunctionTypedefFunc,
+    bool Function(Declaration declaration)? isLeafFunctionFunc,
+    bool Function(Declaration declaration)? enumShouldBeIntFunc,
+    bool Function(Declaration declaration)? unnamedEnumsShouldBeIntFunc,
+    FfiNativeConfig ffiNativeConfig = const FfiNativeConfig(enabled: false),
+    bool ignoreSourceErrors = false,
+    bool formatOutput = true,
+  }) =>
+      ConfigImpl(
+        filename: filename == null ? null : Uri.file(filename.toFilePath()),
+        output: Uri.file(output.toFilePath()),
+        outputObjC:
+            Uri.file(outputObjC?.toFilePath() ?? '${output.toFilePath()}.m'),
+        symbolFile: symbolFile,
+        language: language,
+        entryPoints: entryPoints,
+        shouldIncludeHeaderFunc: shouldIncludeHeaderFunc ?? (_) => true,
+        compilerOpts: compilerOpts,
+        varArgFunctions: varArgFunctions,
+        functionDecl: functionDecl ?? DeclarationFilters.excludeAll,
+        structDecl: structDecl ?? DeclarationFilters.excludeAll,
+        unionDecl: unionDecl ?? DeclarationFilters.excludeAll,
+        enumClassDecl: enumClassDecl ?? DeclarationFilters.excludeAll,
+        unnamedEnumConstants:
+            unnamedEnumConstants ?? DeclarationFilters.excludeAll,
+        globals: globals ?? DeclarationFilters.excludeAll,
+        macroDecl: macroDecl ?? DeclarationFilters.excludeAll,
+        typedefs: typedefs ?? DeclarationFilters.excludeAll,
+        objcInterfaces: objcInterfaces ?? DeclarationFilters.excludeAll,
+        objcProtocols: objcProtocols ?? DeclarationFilters.excludeAll,
+        includeUnusedTypedefs: includeUnusedTypedefs,
+        generateForPackageObjectiveC: generateForPackageObjectiveC,
+        sort: sort,
+        useSupportedTypedefs: useSupportedTypedefs,
+        libraryImports: Map<String, LibraryImport>.fromEntries(
+            libraryImports.map((import) =>
+                MapEntry<String, LibraryImport>(import.name, import))),
+        usrTypeMappings: Map<String, ImportedType>.fromEntries(
+            usrTypeMappings.map((import) =>
+                MapEntry<String, ImportedType>(import.nativeType, import))),
+        typedefTypeMappings: Map<String, ImportedType>.fromEntries(
+            typedefTypeMappings.map((import) =>
+                MapEntry<String, ImportedType>(import.nativeType, import))),
+        structTypeMappings: Map<String, ImportedType>.fromEntries(
+            structTypeMappings.map((import) =>
+                MapEntry<String, ImportedType>(import.nativeType, import))),
+        unionTypeMappings: Map<String, ImportedType>.fromEntries(
+            unionTypeMappings.map((import) =>
+                MapEntry<String, ImportedType>(import.nativeType, import))),
+        nativeTypeMappings: Map<String, ImportedType>.fromEntries(
+            nativeTypeMappings.map((import) =>
+                MapEntry<String, ImportedType>(import.nativeType, import))),
+        commentType: commentType ?? CommentType.def(),
+        structDependencies: structDependencies,
+        unionDependencies: unionDependencies,
+        structPackingOverrideFunc: structPackingOverrideFunc ?? (_) => null,
+        interfaceModuleFunc: interfaceModuleFunc ?? (_) => null,
+        protocolModuleFunc: protocolModuleFunc ?? (_) => null,
+        wrapperName: wrapperName,
+        wrapperDocComment: wrapperDocComment,
+        preamble: preamble,
+        useDartHandle: useDartHandle,
+        silenceEnumWarning: silenceEnumWarning,
+        shouldExposeFunctionTypedefFunc:
+            shouldExposeFunctionTypedefFunc ?? (_) => false,
+        isLeafFunctionFunc: isLeafFunctionFunc ?? (_) => false,
+        enumShouldBeIntFunc: enumShouldBeIntFunc ?? (_) => false,
+        unnamedEnumsShouldBeIntFunc:
+            unnamedEnumsShouldBeIntFunc ?? (_) => false,
+        ffiNativeConfig: ffiNativeConfig,
+        ignoreSourceErrors: ignoreSourceErrors,
+        formatOutput: formatOutput,
+      );
+}
+
+abstract interface class DeclarationFilters {
+  /// Checks if a name is allowed by a filter.
+  bool shouldInclude(Declaration declaration);
+
+  /// Checks if the symbol address should be included for this name.
+  bool shouldIncludeSymbolAddress(Declaration declaration);
+
+  /// Applies renaming and returns the result.
+  String rename(Declaration declaration);
+
+  /// Applies member renaming and returns the result.
+  String renameMember(Declaration declaration, String member);
+
+  factory DeclarationFilters({
+    bool Function(Declaration declaration)? shouldInclude,
+    bool Function(Declaration declaration)? shouldIncludeSymbolAddress,
+    String Function(Declaration declaration)? rename,
+    String Function(Declaration declaration, String member)? renameMember,
+  }) =>
+      DeclarationFiltersImpl(
+        shouldIncludeFunc: shouldInclude ?? (_) => false,
+        shouldIncludeSymbolAddressFunc:
+            shouldIncludeSymbolAddress ?? (_) => false,
+        renameFunc: rename ?? (declaration) => declaration.originalName,
+        renameMemberFunc: renameMember ?? (_, member) => member,
+      );
+
+  static final excludeAll = DeclarationFilters();
+  static final includeAll = DeclarationFilters(shouldInclude: (_) => true);
+}
--- pkgs/ffigenpad/lib/src/config_provider/config_types.dart
+++ pkgs/ffigenpad/lib/src/config_provider/config_types.dart
@@ -2,4 +2,430 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'package:ffigen/src/config_provider/config_types.dart';
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Contains all the neccesary classes required by config.
+library;
+
+import 'package:quiver/pattern.dart' as quiver;
+
+import '../code_generator.dart';
+import 'config.dart';
+
+enum Language { c, objc }
+
+class CommentType {
+  CommentStyle style;
+  CommentLength length;
+  CommentType(this.style, this.length);
+
+  /// Sets default style as [CommentStyle.doxygen], default length as
+  /// [CommentLength.full].
+  CommentType.def()
+      : style = CommentStyle.doxygen,
+        length = CommentLength.full;
+
+  /// Disables any comments.
+  CommentType.none()
+      : style = CommentStyle.doxygen,
+        length = CommentLength.none;
+}
+
+enum CommentStyle { doxygen, any }
+
+enum CommentLength { none, brief, full }
+
+enum CompoundDependencies { full, opaque }
+
+/// Holds config for how Structs Packing will be overriden.
+class StructPackingOverride {
+  final List<(RegExp, int?)> _matchers;
+
+  StructPackingOverride(this._matchers);
+
+  /// Returns pack value for [name].
+  PackingValue? getOverridenPackValue(String name) {
+    for (final (regex, value) in _matchers) {
+      if (quiver.matchesFull(regex, name)) {
+        return PackingValue(value);
+      }
+    }
+    return null;
+  }
+}
+
+// Holds headers and filters for header.
+class YamlHeaders {
+  /// Path to headers.
+  ///
+  /// This contains all the headers, after extraction from Globs.
+  final List<Uri> entryPoints;
+
+  /// Include filter for headers.
+  final HeaderIncludeFilter includeFilter;
+
+  YamlHeaders({List<String>? entryPoints, HeaderIncludeFilter? includeFilter})
+      : entryPoints = entryPoints?.map(Uri.file).toList() ?? [],
+        includeFilter = includeFilter ?? GlobHeaderFilter();
+}
+
+abstract class HeaderIncludeFilter {
+  bool shouldInclude(Uri headerSourceFile);
+}
+
+class GlobHeaderFilter extends HeaderIncludeFilter {
+  List<quiver.Glob>? includeGlobs = [];
+
+  GlobHeaderFilter({
+    this.includeGlobs,
+  });
+
+  @override
+  bool shouldInclude(Uri headerSourceFile) {
+    // Return true if header was included.
+    for (final globPattern in includeGlobs!) {
+      if (quiver.matchesFull(globPattern, headerSourceFile.toFilePath())) {
+        return true;
+      }
+    }
+
+    // If any includedInclusionHeaders is provided, return false.
+    if (includeGlobs!.isNotEmpty) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+/// A generic declaration config, used for Functions, Structs, Enums, Macros,
+/// unnamed Enums and Globals.
+class YamlDeclarationFilters implements DeclarationFilters {
+  final YamlIncluder _includer;
+  final YamlRenamer _renamer;
+  final YamlMemberRenamer _memberRenamer;
+  final YamlIncluder _symbolAddressIncluder;
+  final bool excludeAllByDefault;
+
+  YamlDeclarationFilters({
+    YamlIncluder? includer,
+    YamlRenamer? renamer,
+    YamlMemberRenamer? memberRenamer,
+    YamlIncluder? symbolAddressIncluder,
+    required this.excludeAllByDefault,
+  })  : _includer = includer ?? YamlIncluder(),
+        _renamer = renamer ?? YamlRenamer(),
+        _memberRenamer = memberRenamer ?? YamlMemberRenamer(),
+        _symbolAddressIncluder =
+            symbolAddressIncluder ?? YamlIncluder.excludeByDefault();
+
+  /// Applies renaming and returns the result.
+  @override
+  String rename(Declaration declaration) =>
+      _renamer.rename(declaration.originalName);
+
+  /// Applies member renaming and returns the result.
+  @override
+  String renameMember(Declaration declaration, String member) =>
+      _memberRenamer.rename(declaration.originalName, member);
+
+  /// Checks if a name is allowed by a filter.
+  @override
+  bool shouldInclude(Declaration declaration) =>
+      _includer.shouldInclude(declaration.originalName, excludeAllByDefault);
+
+  /// Checks if the symbol address should be included for this name.
+  @override
+  bool shouldIncludeSymbolAddress(Declaration declaration) =>
+      _symbolAddressIncluder.shouldInclude(declaration.originalName);
+}
+
+/// Matches `$<single_digit_int>`, value can be accessed in group 1 of match.
+final replaceGroupRegexp = RegExp(r'\$([0-9])');
+
+/// Match/rename using [regExp].
+class RegExpRenamer {
+  final RegExp regExp;
+  final String replacementPattern;
+
+  RegExpRenamer(this.regExp, this.replacementPattern);
+
+  /// Returns true if [str] has a full match with [regExp].
+  bool matches(String str) => quiver.matchesFull(regExp, str);
+
+  /// Renames [str] according to [replacementPattern].
+  ///
+  /// Returns [str] if [regExp] doesn't have a full match.
+  String rename(String str) {
+    if (matches(str)) {
+      // Get match.
+      final regExpMatch = regExp.firstMatch(str)!;
+
+      /// Get group values.
+      /// E.g for `str`: `clang_dispose` and `regExp`: `clang_(.*)`
+      /// groups will be `0`: `clang_disponse`, `1`: `dispose`.
+      final groups = regExpMatch.groups(
+          List.generate(regExpMatch.groupCount, (index) => index) +
+              [regExpMatch.groupCount]);
+
+      /// Replace all `$<int>` symbols with respective groups (if any).
+      final result =
+          replacementPattern.replaceAllMapped(replaceGroupRegexp, (match) {
+        final groupInt = int.parse(match.group(1)!);
+        return groups[groupInt]!;
+      });
+      return result;
+    } else {
+      return str;
+    }
+  }
+
+  @override
+  String toString() {
+    return 'Regexp: $regExp, ReplacementPattern: $replacementPattern';
+  }
+}
+
+/// Handles `include/exclude` logic for a declaration.
+class YamlIncluder {
+  final List<RegExp> _includeMatchers;
+  final Set<String> _includeFull;
+  final List<RegExp> _excludeMatchers;
+  final Set<String> _excludeFull;
+
+  YamlIncluder({
+    List<RegExp>? includeMatchers,
+    Set<String>? includeFull,
+    List<RegExp>? excludeMatchers,
+    Set<String>? excludeFull,
+  })  : _includeMatchers = includeMatchers ?? [],
+        _includeFull = includeFull ?? {},
+        _excludeMatchers = excludeMatchers ?? [],
+        _excludeFull = excludeFull ?? {};
+
+  YamlIncluder.excludeByDefault()
+      : _includeMatchers = [],
+        _includeFull = {},
+        _excludeMatchers = [RegExp('.*', dotAll: true)],
+        _excludeFull = {};
+
+  /// Returns true if [name] is allowed.
+  ///
+  /// Exclude overrides include.
+  bool shouldInclude(String name, [bool excludeAllByDefault = false]) {
+    if (_excludeFull.contains(name)) {
+      return false;
+    }
+
+    for (final em in _excludeMatchers) {
+      if (quiver.matchesFull(em, name)) {
+        return false;
+      }
+    }
+
+    if (_includeFull.contains(name)) {
+      return true;
+    }
+
+    for (final im in _includeMatchers) {
+      if (quiver.matchesFull(im, name)) {
+        return true;
+      }
+    }
+
+    // If user has provided 'include' field in the filter, then default
+    // matching is false.
+    if (_includeMatchers.isNotEmpty || _includeFull.isNotEmpty) {
+      return false;
+    } else {
+      // Otherwise, fall back to the default behavior for empty filters.
+      return !excludeAllByDefault;
+    }
+  }
+}
+
+/// Handles `full/regexp` renaming logic.
+class YamlRenamer {
+  final Map<String, String> _renameFull;
+  final List<RegExpRenamer> _renameMatchers;
+
+  YamlRenamer({
+    List<RegExpRenamer>? renamePatterns,
+    Map<String, String>? renameFull,
+  })  : _renameMatchers = renamePatterns ?? [],
+        _renameFull = renameFull ?? {};
+
+  YamlRenamer.noRename()
+      : _renameMatchers = [],
+        _renameFull = {};
+
+  String rename(String name) {
+    // Apply full rename (if any).
+    if (_renameFull.containsKey(name)) {
+      return _renameFull[name]!;
+    }
+
+    // Apply rename regexp (if matches).
+    for (final renamer in _renameMatchers) {
+      if (renamer.matches(name)) {
+        return renamer.rename(name);
+      }
+    }
+
+    // No renaming is provided for this declaration, return unchanged.
+    return name;
+  }
+}
+
+/// Match declaration name using [declarationRegExp].
+class RegExpMemberRenamer {
+  final RegExp declarationRegExp;
+  final YamlRenamer memberRenamer;
+
+  RegExpMemberRenamer(this.declarationRegExp, this.memberRenamer);
+
+  /// Returns true if [declaration] has a full match with [declarationRegExp].
+  bool matchesDeclarationName(String declaration) =>
+      quiver.matchesFull(declarationRegExp, declaration);
+
+  @override
+  String toString() {
+    return 'DeclarationRegExp: $declarationRegExp, '
+        'YamlMemberRenamer: $memberRenamer';
+  }
+}
+
+/// Handles `full/regexp` member renaming.
+class YamlMemberRenamer {
+  final Map<String, YamlRenamer> _memberRenameFull;
+  final List<RegExpMemberRenamer> _memberRenameMatchers;
+
+  final Map<String, YamlRenamer> _cache = {};
+
+  YamlMemberRenamer({
+    Map<String, YamlRenamer>? memberRenameFull,
+    List<RegExpMemberRenamer>? memberRenamePattern,
+  })  : _memberRenameFull = memberRenameFull ?? {},
+        _memberRenameMatchers = memberRenamePattern ?? [];
+
+  String rename(String declaration, String member) {
+    if (_cache.containsKey(declaration)) {
+      return _cache[declaration]!.rename(member);
+    }
+
+    // Apply full rename (if any).
+    if (_memberRenameFull.containsKey(declaration)) {
+      // Add to cache.
+      _cache[declaration] = _memberRenameFull[declaration]!;
+      return _cache[declaration]!.rename(member);
+    }
+
+    // Apply rename regexp (if matches).
+    for (final renamer in _memberRenameMatchers) {
+      if (renamer.matchesDeclarationName(declaration)) {
+        // Add to cache.
+        _cache[declaration] = renamer.memberRenamer;
+        return _cache[declaration]!.rename(member);
+      }
+    }
+
+    // No renaming is provided for this declaration, return unchanged.
+    return member;
+  }
+}
+
+List<String> defaultCompilerOpts({bool macIncludeStdLib = true}) => [];
+
+/// Handles config for automatically added compiler options.
+class CompilerOptsAuto {
+  final bool macIncludeStdLib;
+
+  CompilerOptsAuto({bool? macIncludeStdLib})
+      : macIncludeStdLib = macIncludeStdLib ?? true;
+
+  /// Extracts compiler options based on OS and config.
+  List<String> extractCompilerOpts() {
+    return defaultCompilerOpts(macIncludeStdLib: macIncludeStdLib);
+  }
+}
+
+class _ObjCModuleEntry {
+  final RegExp pattern;
+  final String moduleName;
+
+  _ObjCModuleEntry(this.pattern, this.moduleName);
+}
+
+/// Handles applying module prefixes to ObjC classes.
+class ObjCModules {
+  final _prefixes = <_ObjCModuleEntry>[];
+
+  ObjCModules(Map<String, String> prefixes) {
+    for (final entry in prefixes.entries) {
+      _prefixes.add(_ObjCModuleEntry(RegExp(entry.key), entry.value));
+    }
+  }
+
+  /// If any of the prefixing patterns match, returns the corresponding module.
+  /// Otherwise returns null.
+  String? getModule(String className) {
+    for (final entry in _prefixes) {
+      if (quiver.matchesFull(entry.pattern, className)) {
+        return entry.moduleName;
+      }
+    }
+    return null;
+  }
+}
+
+class FfiNativeConfig {
+  final bool enabled;
+  final String? assetId;
+
+  const FfiNativeConfig({required this.enabled, this.assetId});
+}
+
+class SymbolFile {
+  final Uri importPath;
+  final Uri output;
+
+  SymbolFile(this.importPath, this.output);
+}
+
+class OutputConfig {
+  final String output;
+  final String? outputObjC;
+  final SymbolFile? symbolFile;
+
+  OutputConfig(this.output, this.outputObjC, this.symbolFile);
+}
+
+class RawVarArgFunction {
+  String? postfix;
+  final List<String> rawTypeStrings;
+
+  RawVarArgFunction(this.postfix, this.rawTypeStrings);
+}
+
+class VarArgFunction {
+  final String postfix;
+  final List<Type> types;
+
+  VarArgFunction(this.postfix, this.types);
+}
+
+class PackingValue {
+  int? value;
+  PackingValue(this.value);
+}
+
+class Declaration {
+  String usr;
+  String originalName;
+  Declaration({
+    required this.usr,
+    required this.originalName,
+  });
+}
