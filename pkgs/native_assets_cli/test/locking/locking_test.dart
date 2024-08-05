a98307f80f419bf188e1263ffaf903ef9a341b4c
Daco Harkes <dacoharkes@google.com>
2024-08-05 16:34:00 +0200

    fix formatting

--- pkgs/native_assets_cli/test/locking/locking_test.dart
+++ pkgs/native_assets_cli/test/locking/locking_test.dart
@@ -45,14 +45,14 @@ void main() async {
     if (lockFile.existsSync()) {
       if (!Platform.isWindows) {
         final lockFileContents = lockFile.readAsStringSync();
-          if (lockFileContents.isNotEmpty) {
-            // The process might have been killed in between creating the lock
-            // file and writing to it.
-            expect(
-              lockFileContents,
-              stringContainsInOrder(['Last acquired by']),
-            );
-          }
+        if (lockFileContents.isNotEmpty) {
+          // The process might have been killed in between creating the lock
+          // file and writing to it.
+          expect(
+            lockFileContents,
+            stringContainsInOrder(['Last acquired by']),
+          );
+        }
       }
       return lockFile;
     }
