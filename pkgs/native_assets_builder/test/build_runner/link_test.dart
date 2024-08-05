990b92f6a7592f8aa968b7230ab898cf88dcb1cc
Daco Harkes <dacoharkes@google.com>
2024-08-05 14:04:01 +0200

    undo test change

--- pkgs/native_assets_builder/test/build_runner/link_test.dart
+++ pkgs/native_assets_builder/test/build_runner/link_test.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:native_assets_cli/native_assets_cli.dart' as cli;
 import 'package:native_assets_cli/src/api/asset.dart';
 import 'package:test/test.dart';
@@ -152,8 +150,7 @@ void main() async {
     });
   });
 
-  test('treeshaking assets using CLinker',
-      skip: !Platform.isLinux, timeout: longTimeout, () async {
+  test('treeshaking assets using CLinker', timeout: longTimeout, () async {
     await inTempDir((tempUri) async {
       await copyTestProjects(targetUri: tempUri);
       final packageUri = tempUri.resolve('treeshaking_native_libs/');
