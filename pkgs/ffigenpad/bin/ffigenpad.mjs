17d31606951ca454502991c93a3272c21a3852c5
TheComputerM <mudit.somani00@gmail.com>
2024-08-06 11:31:39 +0530

    [ffigenpad] demo run

--- pkgs/ffigenpad/bin/ffigenpad.mjs
+++ pkgs/ffigenpad/bin/ffigenpad.mjs
@@ -52,6 +52,8 @@ export const instantiate = async (modulePromise, importObjectPromise) => {
     // Imports
     const dart2wasm = {
 
+_56: Date.now,
+_58: s => new Date(s * 1000).getTimezoneOffset() * 60 ,
 _60: () => {
           let stackString = new Error().stack.toString();
           let frames = stackString.split('\n');
@@ -61,10 +63,32 @@ _60: () => {
           }
           return frames.slice(drop).join('\n');
         },
+_64: () => {
+      // On browsers return `globalThis.location.href`
+      if (globalThis.location != null) {
+        return globalThis.location.href;
+      }
+      return null;
+    },
+_65: () => {
+        return typeof process != "undefined" &&
+               Object.prototype.toString.call(process) == "[object process]" &&
+               process.platform == "win32"
+      },
 _79: s => JSON.stringify(s),
 _80: s => printToConsole(s),
 _81: a => a.join(''),
+_82: (o, a, b) => o.replace(a, b),
+_84: (s, t) => s.split(t),
+_85: s => s.toLowerCase(),
+_86: s => s.toUpperCase(),
+_87: s => s.trim(),
 _91: (s, p, i) => s.indexOf(p, i),
+_92: (s, p, i) => s.lastIndexOf(p, i),
+_93: (s) => s.replace(/\$/g, "$$$$"),
+_94: Object.is,
+_95: s => s.toUpperCase(),
+_96: s => s.toLowerCase(),
 _97: (a, i) => a.push(i),
 _108: a => a.length,
 _110: (a, i) => a[i],
@@ -82,6 +106,7 @@ _127: (o) => new DataView(o.buffer, o.byteOffset, o.byteLength),
 _131: Function.prototype.call.bind(Object.getOwnPropertyDescriptor(DataView.prototype, 'byteLength').get),
 _132: (b, o) => new DataView(b, o),
 _134: Function.prototype.call.bind(DataView.prototype.getUint8),
+_135: Function.prototype.call.bind(DataView.prototype.setUint8),
 _136: Function.prototype.call.bind(DataView.prototype.getInt8),
 _138: Function.prototype.call.bind(DataView.prototype.getUint16),
 _140: Function.prototype.call.bind(DataView.prototype.getInt16),
@@ -92,35 +117,55 @@ _152: Function.prototype.call.bind(DataView.prototype.getFloat64),
 _159: f => finalizeWrapper(f, function(x0,x1,x2) { return dartInstance.exports._159(f,arguments.length,x0,x1,x2) }),
 _160: (x0,x1) => globalThis.addFunction(x0,x1),
 _161: x0 => globalThis.removeFunction(x0),
-_185: (c) =>
+_162: (x0,x1) => x0.mkdir(x1),
+_163: (x0,x1,x2) => x0.writeFile(x1,x2),
+_164: (x0,x1) => x0.unlink(x1),
+_165: (x0,x1,x2) => x0.analyzePath(x1,x2),
+_166: (x0,x1,x2) => x0.writeFile(x1,x2),
+_169: x0 => x0.cwd(),
+_180: () => globalThis.FS,
+_204: (c) =>
               queueMicrotask(() => dartInstance.exports.$invokeCallback(c)),
-_199: o => o === undefined,
-_200: o => typeof o === 'boolean',
-_201: o => typeof o === 'number',
-_203: o => typeof o === 'string',
-_206: o => o instanceof Int8Array,
-_207: o => o instanceof Uint8Array,
-_208: o => o instanceof Uint8ClampedArray,
-_209: o => o instanceof Int16Array,
-_210: o => o instanceof Uint16Array,
-_211: o => o instanceof Int32Array,
-_212: o => o instanceof Uint32Array,
-_213: o => o instanceof Float32Array,
-_214: o => o instanceof Float64Array,
-_215: o => o instanceof ArrayBuffer,
-_216: o => o instanceof DataView,
-_217: o => o instanceof Array,
-_218: o => typeof o === 'function' && o[jsWrappedDartFunctionSymbol] === true,
-_222: (l, r) => l === r,
-_223: o => o,
-_224: o => o,
-_225: o => o,
-_226: b => !!b,
-_227: o => o.length,
-_230: (o, i) => o[i],
-_231: f => f.dartFunction,
-_232: l => arrayFromDartList(Int8Array, l),
-_233: (data, length) => {
+_207: (s, m) => {
+          try {
+            return new RegExp(s, m);
+          } catch (e) {
+            return String(e);
+          }
+        },
+_208: (x0,x1) => x0.exec(x1),
+_209: (x0,x1) => x0.test(x1),
+_210: (x0,x1) => x0.exec(x1),
+_211: (x0,x1) => x0.exec(x1),
+_212: x0 => x0.pop(),
+_218: o => o === undefined,
+_219: o => typeof o === 'boolean',
+_220: o => typeof o === 'number',
+_222: o => typeof o === 'string',
+_225: o => o instanceof Int8Array,
+_226: o => o instanceof Uint8Array,
+_227: o => o instanceof Uint8ClampedArray,
+_228: o => o instanceof Int16Array,
+_229: o => o instanceof Uint16Array,
+_230: o => o instanceof Int32Array,
+_231: o => o instanceof Uint32Array,
+_232: o => o instanceof Float32Array,
+_233: o => o instanceof Float64Array,
+_234: o => o instanceof ArrayBuffer,
+_235: o => o instanceof DataView,
+_236: o => o instanceof Array,
+_237: o => typeof o === 'function' && o[jsWrappedDartFunctionSymbol] === true,
+_240: o => o instanceof RegExp,
+_241: (l, r) => l === r,
+_242: o => o,
+_243: o => o,
+_244: o => o,
+_245: b => !!b,
+_246: o => o.length,
+_249: (o, i) => o[i],
+_250: f => f.dartFunction,
+_251: l => arrayFromDartList(Int8Array, l),
+_252: (data, length) => {
           const jsBytes = new Uint8Array(length);
           const getByte = dartInstance.exports.$uint8ListGet;
           for (let i = 0; i < length; i++) {
@@ -128,14 +173,14 @@ _233: (data, length) => {
           }
           return jsBytes;
         },
-_234: l => arrayFromDartList(Uint8ClampedArray, l),
-_235: l => arrayFromDartList(Int16Array, l),
-_236: l => arrayFromDartList(Uint16Array, l),
-_237: l => arrayFromDartList(Int32Array, l),
-_238: l => arrayFromDartList(Uint32Array, l),
-_239: l => arrayFromDartList(Float32Array, l),
-_240: l => arrayFromDartList(Float64Array, l),
-_241: (data, length) => {
+_253: l => arrayFromDartList(Uint8ClampedArray, l),
+_254: l => arrayFromDartList(Int16Array, l),
+_255: l => arrayFromDartList(Uint16Array, l),
+_256: l => arrayFromDartList(Int32Array, l),
+_257: l => arrayFromDartList(Uint32Array, l),
+_258: l => arrayFromDartList(Float32Array, l),
+_259: l => arrayFromDartList(Float64Array, l),
+_260: (data, length) => {
           const read = dartInstance.exports.$byteDataGetUint8;
           const view = new DataView(new ArrayBuffer(length));
           for (let i = 0; i < length; i++) {
@@ -143,8 +188,8 @@ _241: (data, length) => {
           }
           return view;
         },
-_242: l => arrayFromDartList(Array, l),
-_243:       (s, length) => {
+_261: l => arrayFromDartList(Array, l),
+_262:       (s, length) => {
         if (length == 0) return '';
 
         const read = dartInstance.exports.$stringRead1;
@@ -165,7 +210,7 @@ _243:       (s, length) => {
         return result;
       }
       ,
-_244:     (s, length) => {
+_263:     (s, length) => {
       if (length == 0) return '';
 
       const read = dartInstance.exports.$stringRead2;
@@ -186,7 +231,7 @@ _244:     (s, length) => {
       return result;
     }
     ,
-_245:     (s) => {
+_264:     (s) => {
       let length = s.length;
       let range = 0;
       for (let i = 0; i < length; i++) {
@@ -242,10 +287,20 @@ _245:     (s) => {
       }
     }
     ,
-_248: l => new Array(l),
-_252: (o, p) => o[p],
-_256: o => String(o),
-_278: v => v.toString()
+_267: l => new Array(l),
+_271: (o, p) => o[p],
+_275: o => String(o),
+_280: x0 => x0.index,
+_282: x0 => x0.length,
+_284: (x0,x1) => x0[x1],
+_287: x0 => x0.flags,
+_288: x0 => x0.multiline,
+_289: x0 => x0.ignoreCase,
+_290: x0 => x0.unicode,
+_291: x0 => x0.dotAll,
+_292: (x0,x1) => x0.lastIndex = x1,
+_294: (o, p) => o[p],
+_297: v => v.toString()
     };
 
     const baseImports = {
--- pkgs/ffigenpad/lib/ffigenpad.dart
+++ pkgs/ffigenpad/lib/ffigenpad.dart
@@ -2,31 +2,20 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:ffigenpad/src/header_parser/calloc.dart';
-import 'package:ffigenpad/src/header_parser/utils.dart';
-import 'dart:ffi' as ffi;
-import 'package:ffigenpad/src/header_parser/clang_bindings/clang_bindings.dart';
+import 'dart:io';
+
+import 'package:ffigenpad/memfs.dart';
+import 'package:ffigenpad/src/config_provider.dart';
+import 'src/ffigen.dart';
+
+void generate() {
+  final ffigen = FfiGen();
+  final config = Config(
+      output: Uri.file('/output.dart'),
+      entryPoints: [Uri.file('/home/web_user/test.h')]);
+  ffigen.run(config);
+}
 
 void main() {
-  final index = clang_createIndex(1, 1);
-  final filename = "./test.c".toNativeUint8();
-  final args = ["-v"];
-  final argsPointer = calloc<ffi.Pointer<ffi.Uint8>>(args.length);
-  for (int i = 0; i < args.length; i++) {
-    argsPointer[i] = args[i].toNativeUint8();
-  }
-  final translationUnit = clang_parseTranslationUnit(
-    index,
-    filename,
-    argsPointer,
-    1,
-    ffi.nullptr,
-    0,
-    0,
-  );
-  final root = clang_getTranslationUnitCursor_wrap(translationUnit);
-  root.printAst();
-  calloc.free(root);
-  clang_disposeTranslationUnit(translationUnit);
-  clang_disposeIndex(index);
+  IOOverrides.runWithIOOverrides(generate, MemFSIOOverrides());
 }
