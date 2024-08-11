c832daf9b3484161a3dcc98c17d854bb06b7fd50
TheComputerM <mudit.somani00@gmail.com>
2024-08-12 01:45:51 +0530

    [ffigenpad] add more info to navbar

--- pkgs/ffigenpad/web/src/components/navbar.tsx
+++ pkgs/ffigenpad/web/src/components/navbar.tsx
@@ -1,9 +1,11 @@
-import { TbMoon, TbSun } from "solid-icons/tb";
+import { TbBug, TbMoon, TbSun } from "solid-icons/tb";
 import { createEffect, Show } from "solid-js";
-import { Flex } from "styled-system/jsx";
+import { Flex, HStack } from "styled-system/jsx";
 import { $theme } from "~/lib/theme";
 import { Heading } from "./ui/heading";
 import { IconButton } from "./ui/icon-button";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
 const ThemeSwitcher = () => {
   const [darkMode, setDarkMode] = $theme.darkMode;
@@ -27,11 +29,32 @@ const ThemeSwitcher = () => {
 
 export const Navbar = () => {
   return (
-    <Flex justify="space-between" align="center" pt="1" px="4" flexShrink={0}>
-      <Heading as="h1" textStyle="xl">
-        FFIgenPad
-      </Heading>
-      <ThemeSwitcher />
-    </Flex>
+    <HStack justify="space-between" pt="1" px="4" flexShrink={0}>
+      <HStack>
+        <Heading as="h1" textStyle="xl">
+          FFIgenPad
+        </Heading>
+        <Badge>ffigen 14.0.0-wip</Badge>
+        <Badge>libclang 18.1.8</Badge>
+      </HStack>
+      <HStack>
+        <Button
+          size="xs"
+          variant="outline"
+          asChild={(localProps) => (
+            <a
+              {...localProps()}
+              // TODO: url redirects to pull request
+              href="https://github.com/dart-lang/native/pull/1390"
+              target="_blank"
+            >
+              <TbBug />
+              Report a Bug
+            </a>
+          )}
+        ></Button>
+        <ThemeSwitcher />
+      </HStack>
+    </HStack>
   );
 };
