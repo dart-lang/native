9a758a301b0019fb27c11b80481746a44a37bad8
Gabriel Terwesten <gabriel@terwesten.net>
2024-08-12 15:18:07 +0200

    Replace `linked_library` with `native_dynamic_linking`

--- pkgs/native_assets_cli/example/build/native_dynamic_linking/test/math_test.dart
+++ pkgs/native_assets_cli/example/build/native_dynamic_linking/test/math_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:linked_library/math.dart';
+import 'package:native_dynamic_linking/math.dart';
 import 'package:test/test.dart';
 
 void main() {
