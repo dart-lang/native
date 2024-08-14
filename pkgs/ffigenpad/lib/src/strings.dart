a5bb1ab9a789272188d50cf13202bdfde7802dfd
TheComputerM <mudit.somani00@gmail.com>
2024-08-15 02:05:55 +0530

    [ffigenpad] add copyright headers to dart files

--- pkgs/ffigenpad/lib/src/strings.dart
+++ pkgs/ffigenpad/lib/src/strings.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'header_parser/clang_bindings/clang_types.dart' as clang;
 import 'package:ffigen/src/strings.dart';
 
