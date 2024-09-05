421b1234739cb0a170a384172bae362064d28e6a
Gabriel Terwesten <gabriel@terwesten.net>
2024-09-05 16:04:51 +0200

    Use `BuildConfig` to write config file

--- pkgs/native_assets_builder/test/test_data/native_dynamic_linking_test.dart
+++ pkgs/native_assets_builder/test/test_data/native_dynamic_linking_test.dart
@@ -8,6 +8,7 @@
 })
 library;
 
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:native_assets_cli/native_assets_cli_internal.dart';
@@ -35,28 +36,35 @@ void main() async {
       final testPackageUri = testDataUri.resolve('$name/');
       final dartUri = Uri.file(Platform.resolvedExecutable);
 
+      final config = BuildConfigImpl(
+        outputDirectory: tempUri,
+        packageName: name,
+        packageRoot: testPackageUri,
+        targetOS: OSImpl.current,
+        version: HookConfigImpl.latestVersion,
+        linkModePreference: LinkModePreferenceImpl.dynamic,
+        dryRun: dryRun,
+        linkingEnabled: false,
+        targetArchitecture: dryRun ? null : ArchitectureImpl.current,
+        buildMode: dryRun ? null : BuildModeImpl.debug,
+        cCompiler: dryRun
+            ? null
+            : CCompilerConfigImpl(
+                compiler: cc,
+                envScript: envScript,
+                envScriptArgs: envScriptArgs,
+              ),
+      );
+
+      final buildConfigUri = testTempUri.resolve('build_config.json');
+      File.fromUri(buildConfigUri)
+          .writeAsStringSync(jsonEncode(config.toJson()));
+
       final processResult = await Process.run(
         dartUri.toFilePath(),
         [
           'hook/build.dart',
-          '-Dout_dir=${tempUri.toFilePath()}',
-          '-Dpackage_name=$name',
-          '-Dpackage_root=${testPackageUri.toFilePath()}',
-          '-Dtarget_os=${OSImpl.current}',
-          '-Dversion=${HookConfigImpl.latestVersion}',
-          '-Dlink_mode_preference=dynamic',
-          '-Ddry_run=$dryRun',
-          if (!dryRun) ...[
-            '-Dtarget_architecture=${ArchitectureImpl.current}',
-            '-Dbuild_mode=debug',
-            if (cc != null) '-Dcc=${cc!.toFilePath()}',
-            if (envScript != null)
-              '-D${CCompilerConfigImpl.envScriptConfigKeyFull}='
-                  '${envScript!.toFilePath()}',
-            if (envScriptArgs != null)
-              '-D${CCompilerConfigImpl.envScriptArgsConfigKeyFull}='
-                  '${envScriptArgs!.join(' ')}',
-          ],
+          '--config=${buildConfigUri.toFilePath()}',
         ],
         workingDirectory: testPackageUri.toFilePath(),
       );
