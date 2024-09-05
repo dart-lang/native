b18d32fba976a0ba9bc8c777291201f21a87d07b
Gabriel Terwesten <gabriel@terwesten.net>
2024-09-05 16:27:56 +0200

    Add `native_dynamic_linking` to `test_data` (#1437)

---
+++ pkgs/native_assets_builder/test/test_data/native_dynamic_linking_test.dart
@@ -0,0 +1,126 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+@OnPlatform({
+  'mac-os': Timeout.factor(2),
+  'windows': Timeout.factor(10),
+})
+library;
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:native_assets_cli/native_assets_cli_internal.dart';
+import 'package:test/test.dart';
+
+import '../helpers.dart';
+
+void main() async {
+  late Uri tempUri;
+  const name = 'native_dynamic_linking';
+
+  setUp(() async {
+    tempUri = (await Directory.systemTemp.createTemp()).uri;
+  });
+
+  tearDown(() async {
+    await Directory.fromUri(tempUri).delete(recursive: true);
+  });
+
+  for (final dryRun in [true, false]) {
+    final testSuffix = dryRun ? ' dry_run' : '';
+    test('native_dynamic_linking build$testSuffix', () async {
+      final testTempUri = tempUri.resolve('test1/');
+      await Directory.fromUri(testTempUri).create();
+      final testPackageUri = testDataUri.resolve('$name/');
+      final dartUri = Uri.file(Platform.resolvedExecutable);
+
+      final config = BuildConfigImpl(
+        outputDirectory: tempUri,
+        packageName: name,
+        packageRoot: testPackageUri,
+        targetOS: OSImpl.current,
+        version: HookConfigImpl.latestVersion,
+        linkModePreference: LinkModePreferenceImpl.dynamic,
+        dryRun: dryRun,
+        linkingEnabled: false,
+        targetArchitecture: dryRun ? null : ArchitectureImpl.current,
+        buildMode: dryRun ? null : BuildModeImpl.debug,
+        cCompiler: dryRun
+            ? null
+            : CCompilerConfigImpl(
+                compiler: cc,
+                envScript: envScript,
+                envScriptArgs: envScriptArgs,
+              ),
+      );
+
+      final buildConfigUri = testTempUri.resolve('build_config.json');
+      File.fromUri(buildConfigUri)
+          .writeAsStringSync(jsonEncode(config.toJson()));
+
+      final processResult = await Process.run(
+        dartUri.toFilePath(),
+        [
+          'hook/build.dart',
+          '--config=${buildConfigUri.toFilePath()}',
+        ],
+        workingDirectory: testPackageUri.toFilePath(),
+      );
+      if (processResult.exitCode != 0) {
+        print(processResult.stdout);
+        print(processResult.stderr);
+        print(processResult.exitCode);
+      }
+      expect(processResult.exitCode, 0);
+
+      final buildOutputUri = tempUri.resolve('build_output.json');
+      final buildOutput = HookOutputImpl.fromJsonString(
+          await File.fromUri(buildOutputUri).readAsString());
+      final assets = buildOutput.assets;
+      final dependencies = buildOutput.dependencies;
+      if (dryRun) {
+        expect(assets.length, greaterThanOrEqualTo(3));
+        expect(dependencies, <Uri>[]);
+      } else {
+        expect(assets.length, 3);
+        expect(await assets.allExist(), true);
+        expect(
+          dependencies,
+          [
+            testPackageUri.resolve('src/debug.c'),
+            testPackageUri.resolve('src/math.c'),
+            testPackageUri.resolve('src/add.c'),
+          ],
+        );
+
+        final addLibraryPath = assets
+            .firstWhere((asset) => asset.id.endsWith('add.dart'))
+            .file!
+            .toFilePath();
+        final addResult = await runProcess(
+          executable: dartExecutable,
+          arguments: [
+            'run',
+            pkgNativeAssetsBuilderUri
+                .resolve('test/test_data/native_dynamic_linking_add.dart')
+                .toFilePath(),
+            addLibraryPath,
+            '1',
+            '2',
+          ],
+          environment: {
+            // Add the directory containing the linked dynamic libraries to the
+            // PATH so that the dynamic linker can find them.
+            if (Platform.isWindows)
+              'PATH': '${tempUri.toFilePath()};${Platform.environment['PATH']}',
+          },
+          throwOnUnexpectedExitCode: true,
+          logger: logger,
+        );
+        expect(addResult.stdout, 'Adding 1 and 2.\n3\n');
+      }
+    });
+  }
+}
---
+++ pkgs/native_assets_builder/test_data/native_dynamic_linking/hook/build.dart
@@ -0,0 +1,73 @@
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:logging/logging.dart';
+import 'package:native_assets_cli/native_assets_cli.dart';
+import 'package:native_toolchain_c/native_toolchain_c.dart';
+
+void main(List<String> args) async {
+  await build(args, (config, output) async {
+    final logger = Logger('')
+      ..level = Level.ALL
+      ..onRecord.listen((record) => print(record.message));
+
+    final builders = [
+      CBuilder.library(
+        name: 'debug',
+        assetName: 'debug',
+        sources: [
+          'src/debug.c',
+        ],
+      ),
+      CBuilder.library(
+        name: 'math',
+        assetName: 'math',
+        sources: [
+          'src/math.c',
+        ],
+        // TODO(https://github.com/dart-lang/native/issues/190): Use specific
+        // API for linking once available.
+        flags: config.dynamicLinkingFlags('debug'),
+      ),
+      CBuilder.library(
+        name: 'add',
+        assetName: 'add.dart',
+        sources: [
+          'src/add.c',
+        ],
+        // TODO(https://github.com/dart-lang/native/issues/190): Use specific
+        // API for linking once available.
+        flags: config.dynamicLinkingFlags('math'),
+      )
+    ];
+
+    // Note: This builders need to be run sequentially because they depend on
+    // each others output.
+    for (final builder in builders) {
+      await builder.run(
+        config: config,
+        output: output,
+        logger: logger,
+      );
+    }
+  });
+}
+
+extension on BuildConfig {
+  List<String> dynamicLinkingFlags(String libraryName) => switch (targetOS) {
+        OS.macOS => [
+            '-L${outputDirectory.toFilePath()}',
+            '-l$libraryName',
+          ],
+        OS.linux => [
+            r'-Wl,-rpath=$ORIGIN',
+            '-L${outputDirectory.toFilePath()}',
+            '-l$libraryName',
+          ],
+        OS.windows => [
+            outputDirectory.resolve('$libraryName.dll').toFilePath(),
+          ],
+        _ => throw UnimplementedError('Unsupported OS: $targetOS'),
+      };
+}
