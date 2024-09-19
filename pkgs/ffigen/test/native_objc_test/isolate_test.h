6f10a1a705d13ce5e216ba60db8fa86590ca163d
Liam Appelbe <liama@google.com>
2024-09-19 11:39:11 +1000

    Compile isolate_test's ObjC bindings

--- pkgs/ffigen/test/native_objc_test/isolate_test.h
+++ pkgs/ffigen/test/native_objc_test/isolate_test.h
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-#include "isolate_test.h"
-#include "util.h"
+#import <Foundation/NSObject.h>
 
-@implementation Sendable
+typedef void (^Listener)(int32_t);
+
+@interface Sendable : NSObject {}
+@property int32_t value;
++ (Listener)dummyMethodToForceGenerationOfListener;
 @end
