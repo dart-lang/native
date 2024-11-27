c44a575fa5c80903ff978102069b32fe041229ab
Liam Appelbe <liama@google.com>
2024-11-27 11:13:29 +1100

    Support unlabeled parameters

--- pkgs/swift2objc/lib/src/parser/parsers/declaration_parsers/parse_initializer_declaration.dart
+++ pkgs/swift2objc/lib/src/parser/parsers/declaration_parsers/parse_initializer_declaration.dart
@@ -1,10 +1,8 @@
-import '../../../ast/_core/shared/parameter.dart';
 import '../../../ast/declarations/compounds/members/initializer_declaration.dart';
 import '../../_core/json.dart';
 import '../../_core/parsed_symbolgraph.dart';
-import '../../_core/token_list.dart';
 import '../../_core/utils.dart';
-import '../parse_type.dart';
+import 'parse_function_declaration.dart';
 
 InitializerDeclaration parseInitializerDeclaration(
   Json initializerSymbolJson,
@@ -23,7 +21,7 @@ InitializerDeclaration parseInitializerDeclaration(
 
   return InitializerDeclaration(
     id: id,
-    params: parseInitializerParams(declarationFragments, symbolgraph),
+    params: parseFunctionParams(declarationFragments, symbolgraph),
     hasObjCAnnotation: parseSymbolHasObjcAnnotation(initializerSymbolJson),
     isOverriding: parseIsOverriding(initializerSymbolJson),
     isFailable: parseIsFailableInit(id, declarationFragments),
@@ -32,69 +30,3 @@ InitializerDeclaration parseInitializerDeclaration(
 
 bool parseIsFailableInit(String id, Json declarationFragments) =>
     matchFragment(declarationFragments[1], 'text', '?(');
-
-List<Parameter> parseInitializerParams(
-  Json declarationFragments,
-  ParsedSymbolgraph symbolgraph,
-) {
-  // `declarationFragments` describes each part of the initializer declaration,
-  // things like `init` keyword, brackets, spaces, etc. We only care about the
-  // parameter fragments here, and they always appear in this order:
-  // [
-  //   ..., '(',
-  //   externalParam, ' ', internalParam, ': ', type..., ', '
-  //   externalParam, ': ', type..., ', '
-  //   externalParam, ' ', internalParam, ': ', type..., ')'
-  // ]
-  // Note: `internalParam` may or may not exist.
-  //
-  // The following loop attempts to extract parameters from this flat array
-  // while making sure the parameter fragments have the expected order.
-
-  final parameters = <Parameter>[];
-
-  var tokens = TokenList(declarationFragments);
-  final openParen = tokens.indexWhere((tok) => matchFragment(tok, 'text', '('));
-  if (openParen != -1) {
-    tokens = tokens.slice(openParen + 1);
-    String? consume(String kind) {
-      if (tokens.isEmpty) return null;
-      final token = tokens[0];
-      tokens = tokens.slice(1);
-      return getSpellingForKind(token, kind);
-    }
-
-    final malformedInitializerException = Exception(
-      'Malformed initializer at ${declarationFragments.path}',
-    );
-    while (true) {
-      final externalParam = consume('externalParam');
-      if (externalParam == null) throw malformedInitializerException;
-
-      var sep = consume('text');
-      String? internalParam;
-      if (sep == ' ') {
-        internalParam = consume('internalParam');
-        if (internalParam == null) throw malformedInitializerException;
-        sep = consume('text');
-      }
-
-      if (sep != ': ') throw malformedInitializerException;
-      final (type, remainingTokens) = parseType(symbolgraph, tokens);
-      tokens = remainingTokens;
-
-      parameters.add(Parameter(
-        name: externalParam,
-        internalName: internalParam,
-        type: type,
-      ));
-
-      final end = consume('text');
-      if (end == ')') break;
-      if (end != ', ') throw malformedInitializerException;
-    }
-    if (!tokens.isEmpty) throw malformedInitializerException;
-  }
-
-  return parameters;
-}
