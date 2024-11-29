8f090da798aae38b95a7c40bd568e01c27b15236
Hossein Yousefi <yousefi@google.com>
2024-11-29 01:20:22 +0100

    Make everything public, even though should be by default!

--- pkgs/jnigen/test/kotlin_test/kotlin/src/main/kotlin/com/github/dart_lang/jnigen/Nullability.kt
+++ pkgs/jnigen/test/kotlin_test/kotlin/src/main/kotlin/com/github/dart_lang/jnigen/Nullability.kt
@@ -1,79 +1,79 @@
 package com.github.dart_lang.jnigen
 
-class Nullability<T, U: Any>(val t: T, val u: U, var nullableU: U?) {
-    fun hello(): String {
+public class Nullability<T, U: Any>(val t: T, val u: U, var nullableU: U?) {
+    public fun hello(): String {
         return "hello"
     }
 
-    fun nullableHello(returnNull: Boolean): String? {
+    public fun nullableHello(returnNull: Boolean): String? {
         return if (returnNull) null else "hello"
     }
 
-    fun <V: Any> methodGenericEcho(v: V): V {
+    public fun <V: Any> methodGenericEcho(v: V): V {
         return v
     }
 
-    fun <V : Any?> methodGenericNullableEcho(v: V): V {
+    public fun <V : Any?> methodGenericNullableEcho(v: V): V {
         return v
     }
 
-    fun classGenericEcho(u: U): U {
+    public fun classGenericEcho(u: U): U {
         return u
     }
 
-    fun classGenericNullableEcho(t: T): T {
+    public fun classGenericNullableEcho(t: T): T {
         return t
     }
 
-    fun firstOf(list: List<String>): String {
+    public fun firstOf(list: List<String>): String {
         return list.first();
     }
 
-    fun firstOfNullable(list: List<String?>): String? {
+    public fun firstOfNullable(list: List<String?>): String? {
         return list.first();
     }
 
-    fun classGenericFirstOf(list: List<U>): U {
+    public fun classGenericFirstOf(list: List<U>): U {
         return list.first();
     }
 
-    fun classGenericFirstOfNullable(list: List<T>): T {
+    public fun classGenericFirstOfNullable(list: List<T>): T {
         return list.first();
     }
 
-    fun <V: Any> methodGenericFirstOf(list: List<V>): V {
+    public fun <V: Any> methodGenericFirstOf(list: List<V>): V {
         return list.first();
     }
 
-    fun <V> methodGenericFirstOfNullable(list: List<V>): V {
+    public fun <V> methodGenericFirstOfNullable(list: List<V>): V {
         return list.first();
     }
 
-    fun stringListOf(element: String): List<String> {
+    public fun stringListOf(element: String): List<String> {
         return listOf(element)
     }
 
-    fun nullableListOf(element: String?): List<String?> {
+    public fun nullableListOf(element: String?): List<String?> {
         return listOf(element)
     }
 
-    fun classGenericListOf(element: U): List<U> {
+    public fun classGenericListOf(element: U): List<U> {
         return listOf(element)
     }
 
-    fun classGenericNullableListOf(element: T): List<T> {
+    public fun classGenericNullableListOf(element: T): List<T> {
         return listOf(element)
     }
 
-    fun <V: Any> methodGenericListOf(element: V): List<V> {
+    public fun <V: Any> methodGenericListOf(element: V): List<V> {
         return listOf(element)
     }
 
-    fun <V> methodGenericNullableListOf(element: V): List<V> {
+    public fun <V> methodGenericNullableListOf(element: V): List<V> {
         return listOf(element)
     }
 
-    inner class InnerClass<V> {
-        fun f(t: T, u: U, v: V) {}
+    public inner class InnerClass<V> {
+        public fun f(t: T, u: U, v: V) {}
     }
 }
