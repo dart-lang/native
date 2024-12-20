c2a9633615fd68505a064b4de724ff1e9935446f
Daco Harkes <dacoharkes@google.com>
2024-12-20 13:24:20 +0100

    bump copyright year

--- pkgs/native_assets_builder/test_data/native_add_version_skew/src/native_add.c
+++ pkgs/native_assets_builder/test_data/native_add_version_skew/src/native_add.c
@@ -1,9 +1,7 @@
-// Copyright (c) 2023, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2024, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 #include "native_add.h"
 
-int32_t add(int32_t a, int32_t b) {
-   return a + b;
-}
+int32_t add(int32_t a, int32_t b) { return a + b; }
