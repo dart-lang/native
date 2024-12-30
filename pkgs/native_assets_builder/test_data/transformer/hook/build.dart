98c535c22fae72f49aa17343511eb79569f436a5
Daco Harkes <dacoharkes@google.com>
2024-12-30 19:20:00 +0100

    [native_assets_builder] Fix flaky test

--- pkgs/native_assets_builder/test_data/transformer/hook/build.dart
+++ pkgs/native_assets_builder/test_data/transformer/hook/build.dart
@@ -2,8 +2,10 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:crypto/crypto.dart';
 import 'package:native_assets_cli/data_assets.dart';
 import 'package:transformer/src/transform.dart';
 
@@ -17,6 +19,14 @@ void main(List<String> arguments) async {
     var transformedFiles = 0;
     var cachedFiles = 0;
 
+    final hashesFile =
+        File.fromUri(config.outputDirectoryShared.resolve('hashes.json'));
+    final hashes = await hashesFile.exists()
+        ? (json.decoder.convert(await hashesFile.readAsString()) as Map)
+            .cast<String, String>()
+        : <String, String>{};
+    final newHashes = <String, String>{};
+
     await for (final sourceFile in dataDirectory.list()) {
       if (sourceFile is! File) {
         continue;
@@ -24,14 +34,17 @@ void main(List<String> arguments) async {
 
       final sourceName =
           sourceFile.uri.pathSegments.lastWhere((e) => e.isNotEmpty);
+      final sourceContents = await sourceFile.readAsString();
+      final sourceHash = md5.convert(sourceContents.codeUnits).toString();
+      newHashes[sourceName] = sourceHash;
+      final prevHash = hashes[sourceName];
+
       final name = sourceName.replaceFirst('data', 'data_transformed');
       final targetFile = File.fromUri(config.outputDirectoryShared
           .resolve(sourceName.replaceFirst('data', 'data_transformed')));
 
       // TODO(dacoharkes): Timestamps are not enough for correct caching.
-      if (!await targetFile.exists() ||
-          !(await sourceFile.lastModified())
-              .isBefore(await targetFile.lastModified())) {
+      if (!await targetFile.exists() || sourceHash != prevHash) {
         await transformFile(sourceFile, targetFile);
         transformedFiles++;
       } else {
@@ -49,6 +62,8 @@ void main(List<String> arguments) async {
         sourceFile.uri,
       );
     }
+
+    await hashesFile.writeAsString(json.encoder.convert(newHashes));
 
     print('Transformed $transformedFiles files.');
     print('Reused $cachedFiles cached files.');
