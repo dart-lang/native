bc7ec7259ab12e3ffe458890355feb83aa30864b
Levi Lesches <levilesches@gmail.com>
2025-01-14 19:30:03 -0500

    Added native_assets_cli/example/* to workspace

--- pkgs/native_assets_cli/example/build/native_dynamic_linking/test/add_test.dart
+++ pkgs/native_assets_cli/example/build/native_dynamic_linking/test/add_test.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:native_dynamic_linking/add.dart';
+import 'package:native_dynamic_linking_example/add.dart';
 import 'package:test/test.dart';
 
 void main() {
