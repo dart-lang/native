434db67915880b898a78d11d3e740cff9958ac74
Daco Harkes <dacoharkes@google.com>
2025-01-17 11:46:13 +0100

    [native_toolchain_c] Support Clang on Windows (#1893)

--- pkgs/native_toolchain_c/test/cbuilder/cbuilder_cross_windows_host_test.dart
+++ pkgs/native_toolchain_c/test/cbuilder/cbuilder_cross_windows_host_test.dart
@@ -11,19 +11,36 @@ library;
 import 'dart:io';
 
 import 'package:native_toolchain_c/native_toolchain_c.dart';
+import 'package:native_toolchain_c/src/native_toolchain/clang.dart';
 import 'package:native_toolchain_c/src/native_toolchain/msvc.dart';
 import 'package:native_toolchain_c/src/utils/run_process.dart';
 import 'package:test/test.dart';
 
 import '../helpers.dart';
 
-void main() {
+void main() async {
   if (!Platform.isWindows) {
     // Avoid needing status files on Dart SDK CI.
     return;
   }
 
+  final compilers = {
+    // Either provided to be MSVC or null which defaults to MSVC.
+    msvc: () async => cCompiler,
+    // Clang on Windows.
+    clang: () async => CCompilerConfig(
+          archiver:
+              (await llvmAr.defaultResolver!.resolve(logger: logger)).first.uri,
+          compiler:
+              (await clang.defaultResolver!.resolve(logger: logger)).first.uri,
+          linker:
+              (await lld.defaultResolver!.resolve(logger: logger)).first.uri,
+        )
+  };
+
   const targets = [
+    // TODO(https://github.com/dart-lang/native/issues/170): Support arm64.
+    // Architecture.arm64,
     Architecture.ia32,
     Architecture.x64,
   ];
@@ -36,86 +53,95 @@ void main() {
   });
 
   const dumpbinMachine = {
+    Architecture.arm64: 'ARM64',
     Architecture.ia32: 'x86',
     Architecture.x64: 'x64',
   };
 
   const optimizationLevels = OptimizationLevel.values;
   var selectOptimizationLevel = 0;
+  var selectBuildMode = 0;
 
   final dumpbinFileType = {
     DynamicLoadingBundled(): 'DLL',
     StaticLinking(): 'LIBRARY',
   };
 
-  for (final linkMode in [DynamicLoadingBundled(), StaticLinking()]) {
-    for (final target in targets) {
-      // Cycle through all optimization levels.
-      final optimizationLevel = optimizationLevels[selectOptimizationLevel];
-      selectOptimizationLevel =
-          (selectOptimizationLevel + 1) % optimizationLevels.length;
-      test('CBuilder $linkMode library $target $optimizationLevel', () async {
-        final tempUri = await tempDirForTest();
-        final tempUri2 = await tempDirForTest();
-        final addCUri =
-            packageUri.resolve('test/cbuilder/testfiles/add/src/add.c');
-        const name = 'add';
+  for (final compiler in compilers.keys) {
+    for (final linkMode in [DynamicLoadingBundled(), StaticLinking()]) {
+      for (final target in targets) {
+        // Cycle through all optimization levels.
+        final optimizationLevel = optimizationLevels[selectOptimizationLevel];
+        selectOptimizationLevel =
+            (selectOptimizationLevel + 1) % optimizationLevels.length;
+        final buildMode = BuildMode.values[selectBuildMode];
+        selectBuildMode = (selectBuildMode + 1) % BuildMode.values.length;
+        test(
+            'CBuilder ${compiler.name} $linkMode library $target'
+            ' $optimizationLevel $buildMode', () async {
+          final tempUri = await tempDirForTest();
+          final tempUri2 = await tempDirForTest();
+          final addCUri =
+              packageUri.resolve('test/cbuilder/testfiles/add/src/add.c');
+          const name = 'add';
 
-        final buildInputBuilder = BuildInputBuilder()
-          ..setupShared(
-            packageName: name,
-            packageRoot: tempUri,
-            outputFile: tempUri.resolve('output.json'),
-            outputDirectory: tempUri,
-            outputDirectoryShared: tempUri2,
-          )
-          ..config.setupBuild(
-            linkingEnabled: false,
-            dryRun: false,
-          )
-          ..config.setupShared(buildAssetTypes: [CodeAsset.type])
-          ..config.setupCode(
-            targetOS: OS.windows,
-            targetArchitecture: target,
-            linkModePreference: linkMode == DynamicLoadingBundled()
-                ? LinkModePreference.dynamic
-                : LinkModePreference.static,
-            cCompiler: cCompiler,
+          final buildInputBuilder = BuildInputBuilder()
+            ..setupShared(
+              packageName: name,
+              packageRoot: tempUri,
+              outputFile: tempUri.resolve('output.json'),
+              outputDirectory: tempUri,
+              outputDirectoryShared: tempUri2,
+            )
+            ..config.setupBuild(
+              linkingEnabled: false,
+              dryRun: false,
+            )
+            ..config.setupShared(buildAssetTypes: [CodeAsset.type])
+            ..config.setupCode(
+              targetOS: OS.windows,
+              targetArchitecture: target,
+              linkModePreference: linkMode == DynamicLoadingBundled()
+                  ? LinkModePreference.dynamic
+                  : LinkModePreference.static,
+              cCompiler: await (compilers[compiler]!)(),
+            );
+
+          final buildInput = BuildInput(buildInputBuilder.json);
+          final buildOutput = BuildOutputBuilder();
+
+          final cbuilder = CBuilder.library(
+            name: name,
+            assetName: name,
+            sources: [addCUri.toFilePath()],
+            optimizationLevel: optimizationLevel,
+            buildMode: buildMode,
+          );
+          await cbuilder.run(
+            input: buildInput,
+            output: buildOutput,
+            logger: logger,
           );
 
-        final buildInput = BuildInput(buildInputBuilder.json);
-        final buildOutput = BuildOutputBuilder();
-
-        final cbuilder = CBuilder.library(
-          name: name,
-          assetName: name,
-          sources: [addCUri.toFilePath()],
-          optimizationLevel: optimizationLevel,
-          buildMode: BuildMode.release,
-        );
-        await cbuilder.run(
-          input: buildInput,
-          output: buildOutput,
-          logger: logger,
-        );
-
-        final libUri =
-            tempUri.resolve(OS.windows.libraryFileName(name, linkMode));
-        expect(await File.fromUri(libUri).exists(), true);
-        final result = await runProcess(
-          executable: dumpbinUri,
-          arguments: ['/HEADERS', libUri.toFilePath()],
-          logger: logger,
-        );
-        expect(result.exitCode, 0);
-        final machine =
-            result.stdout.split('\n').firstWhere((e) => e.contains('machine'));
-        expect(machine, contains(dumpbinMachine[target]));
-        final fileType = result.stdout
-            .split('\n')
-            .firstWhere((e) => e.contains('File Type'));
-        expect(fileType, contains(dumpbinFileType[linkMode]));
-      });
+          final libUri =
+              tempUri.resolve(OS.windows.libraryFileName(name, linkMode));
+          expect(await File.fromUri(libUri).exists(), true);
+          final result = await runProcess(
+            executable: dumpbinUri,
+            arguments: ['/HEADERS', libUri.toFilePath()],
+            logger: logger,
+          );
+          expect(result.exitCode, 0);
+          final machine = result.stdout
+              .split('\n')
+              .firstWhere((e) => e.contains('machine'));
+          expect(machine, contains(dumpbinMachine[target]));
+          final fileType = result.stdout
+              .split('\n')
+              .firstWhere((e) => e.contains('File Type'));
+          expect(fileType, contains(dumpbinFileType[linkMode]));
+        });
+      }
     }
   }
 }
