59d33c84acbc6106ab17e20dcf57ce09360972e0
Nike Okoronkwo <nikechukwu@gmail.com>
2025-03-10 20:54:31 -0400

    Removed TODO

--- pkgs/swift2objc/lib/src/transformer/transform.dart
+++ pkgs/swift2objc/lib/src/transformer/transform.dart
@@ -14,9 +14,6 @@ import '_core/unique_namer.dart';
 import 'transformers/transform_compound.dart';
 import 'transformers/transform_globals.dart';
 
-// TODO: Update TransformationMap to make referencing types easy.
-//  Would want to just add wrapper suffix but due to unique naming,
-//  can't take chances
 typedef TransformationMap = Map<Declaration, Declaration>;
 
 Set<Declaration> generateDependencies(Iterable<Declaration> decls) =>
