74240033f3a3759800fd9a7d16602e7b14e840bb
MichealReed <micheal.reed@outlook.com>
2025-03-18 13:21:52 -0500

    restore base config.dart imports

--- pkgs/native_assets_cli/lib/src/config.dart
+++ pkgs/native_assets_cli/lib/src/config.dart
@@ -8,10 +8,10 @@ import 'dart:io';
 import 'package:crypto/crypto.dart' show sha256;
 import 'package:pub_semver/pub_semver.dart';
 
-import '../data_assets.dart';
 import 'api/deprecation_messages.dart';
 import 'encoded_asset.dart';
 import 'hook/syntax.g.dart' as syntax;
+import 'metadata.dart';
 import 'utils/datetime.dart';
 import 'utils/json.dart';
 
