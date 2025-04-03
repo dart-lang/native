5f12140c24272831da4c8b813b471197437953fd
Daco Harkes <dacoharkes@google.com>
2025-04-03 11:40:19 +0200

    [native_assets_cli] Syntax for `assets_for_build` and `hooks/metadata`

--- pkgs/hooks/test/schema/schema_test.dart
+++ pkgs/hooks/test/schema/schema_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:json_schema/json_schema.dart';
+
 import 'helpers.dart';
 
 void main() {
@@ -19,6 +21,35 @@ void main() {
     allTestData: allTestData,
     packageUri: packageUri,
   );
+
+  testFields(
+    allSchemas: allSchemas,
+    allTestData: allTestData,
+    packageUri: packageUri,
+    fields: _metadataAssetFields,
+  );
 }
 
 Uri packageUri = findPackageRoot('hooks');
+
+List<(List<Object>, void Function(ValidationResults result))>
+_metadataAssetFields({
+  required InputOrOutput inputOrOutput,
+  required Hook hook,
+  required Party party,
+}) => <(List<Object>, void Function(ValidationResults result))>[
+  for (final path in [
+    if (inputOrOutput == InputOrOutput.input && hook == Hook.link) ['assets'],
+    if (inputOrOutput == InputOrOutput.output && hook == Hook.build) ...[
+      ['assets_for_build'],
+      ['assetsForLinking', 'package_with_linker'],
+      ['assets_for_linking', 'package_with_linker'],
+    ],
+  ]) ...[
+    ([...path, 1], expectOptionalFieldMissing),
+    ([...path, 1, 'type'], expectRequiredFieldMissing),
+    ([...path, 1, 'encoding'], expectRequiredFieldMissing),
+    ([...path, 1, 'encoding', 'key'], expectRequiredFieldMissing),
+    // Skip `encoding.value`, it's optional and may have any type.
+  ],
+];
