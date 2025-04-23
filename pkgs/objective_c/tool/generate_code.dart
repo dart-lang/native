b82aea777d592fc53b58e8880692c04a1da92875
Liam Appelbe <liama@google.com>
2025-04-23 14:50:08 +1000

    nit

--- pkgs/objective_c/tool/generate_code.dart
+++ pkgs/objective_c/tool/generate_code.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// Runs the ffigen configs, then merges tool/data/extra_methods.dart.in and
-// tool/data/extra_methods.dart.in into the Objective C bindings.
+// Runs the ffigen configs, then merges tool/data/extra_methods.dart.in into the
+// Objective C bindings.
 
 // ignore_for_file: avoid_print
 
