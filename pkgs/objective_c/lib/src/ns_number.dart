78f74d7d2e96a680e92d241b4b94475f0e334a2f
Liam Appelbe <liama@google.com>
2025-04-29 11:14:28 +1000

    Use direct constructors

--- pkgs/objective_c/lib/src/ns_number.dart
+++ pkgs/objective_c/lib/src/ns_number.dart
@@ -6,19 +6,19 @@ import 'package:ffi/ffi.dart';
 import 'objective_c_bindings_generated.dart';
 
 extension IntToNSNumber on int {
-  NSNumber toNSNumber() => NSNumber.alloc().initWithLongLong_(this);
+  NSNumber toNSNumber() => NSNumberCreation.numberWithLongLong_(this);
 }
 
 extension DoubleToNSNumber on double {
-  NSNumber toNSNumber() => NSNumber.alloc().initWithDouble_(this);
+  NSNumber toNSNumber() => NSNumberCreation.numberWithDouble_(this);
 }
 
 extension NumToNSNumber on num {
   NSNumber toNSNumber() {
     final value = this;
     return switch (value) {
-      int() => NSNumber.alloc().initWithLongLong_(value),
-      double() => NSNumber.alloc().initWithDouble_(value),
+      int() => NSNumberCreation.numberWithLongLong_(value),
+      double() => NSNumberCreation.numberWithDouble_(value),
     };
   }
 }
