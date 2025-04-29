cb5bf1b734ac04da35bf15bfd9c32bdd18984b37
Liam Appelbe <liama@google.com>
2025-04-30 11:12:49 +1200

    [ffigen] Conversions from Dart numbers to NSNumber (#2254)

---
+++ pkgs/objective_c/test/nsnumber_test.dart
@@ -0,0 +1,53 @@
+// Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Objective C support is only available on mac.
+@TestOn('mac-os')
+library;
+
+import 'dart:ffi';
+
+import 'package:objective_c/objective_c.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('NSNumber', () {
+    setUpAll(() {
+      // TODO(https://github.com/dart-lang/native/issues/1068): Remove this.
+      DynamicLibrary.open('test/objective_c.dylib');
+    });
+
+    test('from double', () {
+      final n = 1.23.toNSNumber();
+
+      expect(n.intValue, 1);
+      expect(n.longLongValue, 1);
+      expect(n.doubleValue, 1.23);
+    });
+
+    test('from int', () {
+      final n = 0x7fffffffffffffff.toNSNumber();
+
+      expect(n.intValue, -1);
+      expect(n.longLongValue, 0x7fffffffffffffff);
+      expect(n.doubleValue, 0x7ffffffffffffff0);
+    });
+
+    test('from num', () {
+      late num x;
+
+      x = 1.23;
+      final n = x.toNSNumber();
+      expect(n.intValue, 1);
+      expect(n.longLongValue, 1);
+      expect(n.doubleValue, 1.23);
+
+      x = 0x7fffffffffffffff;
+      final m = x.toNSNumber();
+      expect(m.intValue, -1);
+      expect(m.longLongValue, 0x7fffffffffffffff);
+      expect(m.doubleValue, 0x7ffffffffffffff0);
+    });
+  });
+}
