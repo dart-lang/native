9d3c742a04412f529f4bac13c61c7e1096805c89
dacoharkes <dacoharkes@google.com>
2025-05-05 18:50:45 +0000

    [native_toolchain_c] Make compatible with Firebase Studio

---
+++ .idx/dev.nix
@@ -0,0 +1,30 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  channel = "stable-24.11";
+  # Use https://search.nixos.org/packages to find packages.
+  packages = [
+    pkgs.android-tools
+    pkgs.clang
+    pkgs.gcc
+    pkgs.jdk17
+    pkgs.llvm
+    pkgs.llvmPackages.bintools
+    # pkgs.pkgsCross.aarch64-android-prebuilt.stdenv.cc # Takes forever to build
+    # pkgs.pkgsCross.aarch64-multiplatform.gcc # Takes forever to build
+  ];
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id".
+    extensions = [
+      "Dart-Code.dart-code"
+      "mhutchie.git-graph"
+      "rangav.vscode-thunder-client"
+    ];
+    workspace = {
+      onCreate = {
+        dart-install = "dart pub get";
+      };
+    };
+  };
+}
