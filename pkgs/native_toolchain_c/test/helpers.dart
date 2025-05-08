ce9f52f25cf13461af42e935aa81519a35acb81e
Daco Harkes <dacoharkes@google.com>
2025-05-08 15:40:37 +0200

    [infra] Enable running tests from root

--- pkgs/native_toolchain_c/test/helpers.dart
+++ pkgs/native_toolchain_c/test/helpers.dart
@@ -91,8 +91,8 @@ Logger _createTestLogger({List<String>? capturedMessages}) =>
 Uri findPackageRoot(String packageName) {
   final script = Platform.script;
   final fileName = script.name;
-  if (fileName.endsWith('_test.dart')) {
-    // We're likely running from source.
+  if (fileName.endsWith('.dart')) {
+    // We're likely running from source in the package somewhere.
     var directory = script.resolve('.');
     while (true) {
       final dirName = directory.name;
@@ -104,11 +104,18 @@ Uri findPackageRoot(String packageName) {
       directory = parent;
     }
   } else if (fileName.endsWith('.dill')) {
+    // Probably from the package root.
     final cwd = Directory.current.uri;
     final dirName = cwd.name;
     if (dirName == packageName) {
       return cwd;
     }
+  }
+  // Or the workspace root.
+  final cwd = Directory.current.uri;
+  final candidate = cwd.resolve('pkgs/$packageName/');
+  if (Directory.fromUri(candidate).existsSync()) {
+    return candidate;
   }
   throw StateError(
     "Could not find package root for package '$packageName'. "
