e4d482a9ff0fb70362b3396bf77c9d74f1dcf591
Liam Appelbe <liama@google.com>
2025-05-16 12:41:03 +1000

    fmt

--- pkgs/ffigen/test/example_tests/objective_c_example_test.dart
+++ pkgs/ffigen/test/example_tests/objective_c_example_test.dart
@@ -29,8 +29,7 @@ void main() {
       expect(output, contains('class AVAudioPlayer extends objc.NSObject {'));
       expect(
           output,
-          contains(
-              'AVAudioPlayer? initWithContentsOfURL(objc.NSURL url, '
+          contains('AVAudioPlayer? initWithContentsOfURL(objc.NSURL url, '
               '{required ffi.Pointer<ffi.Pointer<objc.ObjCObject>> error}) {'));
       expect(output, contains('double get duration {'));
       expect(output, contains('bool play() {'));
