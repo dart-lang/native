a31d8726106c4ec928aabe9f1e61c03a80dafc42
Liam Appelbe <liama@google.com>
2025-06-03 15:57:31 +1000

    [ffigen] Remove an old TODO that filtered certain methods from package:objective_c (#2335)

--- pkgs/objective_c/test/ns_input_stream_test.dart
+++ pkgs/objective_c/test/ns_input_stream_test.dart
@@ -14,6 +14,8 @@ import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
 import 'package:objective_c/objective_c.dart';
+import 'package:objective_c/src/objective_c_bindings_generated.dart'
+    show DartInputStreamAdapter;
 import 'package:test/test.dart';
 
 import 'util.dart';
