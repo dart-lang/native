a825481423964d0d9789b858e18460a64c729cbc
Marshelino Maged <marshelinomaged@gmail.com>
2025-06-05 17:29:47 +0300

    Add  license

--- pkgs/native_doc_dartifier/test/public_abstractor_test.dart
+++ pkgs/native_doc_dartifier/test/public_abstractor_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:native_doc_dartifier/src/public_abstractor.dart';
 import 'package:test/test.dart';
