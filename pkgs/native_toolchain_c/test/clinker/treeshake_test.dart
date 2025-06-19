feb34f6b9f4fc0c5ef6ff818831381a2538b22d8
Michael Goderbauer <goderbauer@google.com>
2025-06-19 04:47:54 -0700

    unused imports

--- pkgs/native_toolchain_c/test/clinker/treeshake_test.dart
+++ pkgs/native_toolchain_c/test/clinker/treeshake_test.dart
@@ -2,10 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
-
 import 'package:code_assets/code_assets.dart';
-import 'package:test/test.dart';
 
 import '../helpers.dart';
 import 'treeshake_helper.dart';
