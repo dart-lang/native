692c2da40f7790a62dad165c06074bc6fd082f51
Brian Quinlan <brian@sweetapp.com>
2025-07-21 16:47:06 -0700

    [objective_c] Fix a bug where `toNSInputStream` would keep listening to events unless `NSInputStream.close` is called (#2392)

--- pkgs/objective_c/lib/src/ns_input_stream.dart
+++ pkgs/objective_c/lib/src/ns_input_stream.dart
@@ -2,9 +2,21 @@ import 'dart:async';
 import 'dart:ffi';
 import 'dart:isolate';
 
-import 'ns_data.dart';
-import 'ns_string.dart';
+import '../objective_c.dart';
+
 import 'objective_c_bindings_generated.dart';
+
+@Native<Pointer<Void> Function()>(
+  isLeaf: true,
+  symbol: 'objc_autoreleasePoolPush',
+)
+external Pointer<Void> _autoreleasePoolPush();
+
+@Native<Void Function(Pointer<Void>)>(
+  isLeaf: true,
+  symbol: 'objc_autoreleasePoolPop',
+)
+external void _autoreleasePoolPop(Pointer<Void> pool);
 
 extension NSInputStreamStreamExtension on Stream<List<int>> {
   /// Return a [NSInputStream] that, when read, will contain the contents of
@@ -15,23 +27,57 @@ extension NSInputStreamStreamExtension on Stream<List<int>> {
   /// > than the one that calls [toNSInputStream]. Otherwise,
   /// > [NSInputStream.read] will deadlock waiting for data to be added from the
   /// > [Stream].
+  ///
+  /// > [!IMPORTANT]
+  /// > [NSInputStream.read] must be called from a different thread or [Isolate]
+  /// > than the one that calls `toNSInputStream`. Otherwise,
+  /// > [NSInputStream.read] will deadlock waiting for data to be added from the
+  /// > [Stream].
+  ///
+  /// > [!IMPORTANT]
+  /// > `toNSInputStream` creates a reference cycle between Dart and
+  /// > Objective-C. Unless this cycle is broken, the [Isolate] calling
+  /// > `toNSInputStream` will never exit. The cycle can be broken by calling
+  /// > [NSInputStream.close] or releasing the `NSInputStream` using
+  /// > `NSInputStream.ref.release()`.
   NSInputStream toNSInputStream() {
     // Eagerly add data until `maxReadAheadSize` is buffered.
     const maxReadAheadSize = 4096;
 
     final port = ReceivePort();
-    final inputStream = DartInputStreamAdapter.inputStreamWithPort(
-      port.sendPort.nativePort,
-    );
+
+    final DartInputStreamAdapter inputStream;
+    final DartInputStreamAdapterWeakHolder weakInputStream;
+
+    // Only hold a weak reference to the returned `inputStream` so that there is
+    // no unbreakable reference cycle between Dart and Objective-C. When the
+    // `inputStream`'s `dealloc` method is called then it sends this code a
+    // message saying that it was closed.
+    final pool = _autoreleasePoolPush();
+    try {
+      inputStream = DartInputStreamAdapter.inputStreamWithPort(
+        port.sendPort.nativePort,
+      );
+      weakInputStream =
+          DartInputStreamAdapterWeakHolder.holderWithInputStreamAdapter(
+            inputStream,
+          );
+    } finally {
+      _autoreleasePoolPop(pool);
+    }
+
     late final StreamSubscription<dynamic> dataSubscription;
 
     dataSubscription = listen(
       (data) {
+        final inputStream = weakInputStream.adapter;
         if (inputStream.addData(data.toNSData()) > maxReadAheadSize) {
           dataSubscription.pause();
         }
+        inputStream.ref.release();
       },
       onError: (Object e) {
+        final inputStream = weakInputStream.adapter;
         final d = NSMutableDictionary();
         d[NSLocalizedDescriptionKey] = e.toString().toNSString();
         inputStream.setError(
@@ -41,10 +87,13 @@ extension NSInputStreamStreamExtension on Stream<List<int>> {
             userInfo: d,
           ),
         );
+        inputStream.ref.release();
         port.close();
       },
       onDone: () {
+        final inputStream = weakInputStream.adapter;
         inputStream.setDone();
+        inputStream.ref.release();
         port.close();
       },
       cancelOnError: true,
