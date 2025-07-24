e9f65d372967d57918ce6a2069d9f194ece766dc
Liam Appelbe <liama@google.com>
2025-07-24 11:52:02 +1000

    fix block test

--- pkgs/ffigen/test/native_objc_test/block_test.dart
+++ pkgs/ffigen/test/native_objc_test/block_test.dart
@@ -20,7 +20,7 @@ import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 import '../test_utils.dart';
-import 'block_bindings.dart';
+import 'block_bindings.dart' hide ObjCBlock_ffiVoid, ObjCBlock_ffiVoid_CallExtension;
 import 'util.dart';
 
 typedef IntBlock = ObjCBlock_Int32_Int32;
