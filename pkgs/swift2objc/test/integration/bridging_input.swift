83b6b8447af6b6de70d9a326e2be03a994a41c9a
Liam Appelbe <liama@google.com>
2025-08-11 10:35:06 +1000

    [swift2objc] Add toll free bridged types (#2486)

---
+++ pkgs/swift2objc/test/integration/bridging_input.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+// TODO(https://github.com/dart-lang/native/issues/2485): Uncomment the Array,
+// Dictionary, and Set functions.
+
+public func funcAffineTransform() -> AffineTransform? { return nil; }
+// public func funcArray() -> Array<String>? { return nil; }
+public func funcCalendar() -> Calendar? { return nil; }
+public func funcCharacterSet() -> CharacterSet? { return nil; }
+public func funcData() -> Data? { return nil; }
+public func funcDate() -> Date? { return nil; }
+public func funcDateComponents() -> DateComponents? { return nil; }
+public func funcDateInterval() -> DateInterval? { return nil; }
+// public func funcDictionary() -> Dictionary<String, String>? { return nil; }
+public func funcIndexPath() -> IndexPath? { return nil; }
+public func funcIndexSet() -> IndexSet? { return nil; }
+public func funcLocale() -> Locale? { return nil; }
+public func funcNotification() -> Notification? { return nil; }
+public func funcNSError() -> NSError? { return nil; }
+// public func funcSet() -> Set<String>? { return nil; }
+public func funcString() -> String? { return nil; }
+public func funcTimeZone() -> TimeZone? { return nil; }
+public func funcURL() -> URL? { return nil; }
+public func funcURLComponents() -> URLComponents? { return nil; }
+public func funcURLQueryItem() -> URLQueryItem? { return nil; }
+public func funcURLRequest() -> URLRequest? { return nil; }
+public func funcUUID() -> UUID? { return nil; }
