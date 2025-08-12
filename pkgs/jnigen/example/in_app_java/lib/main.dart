a4ec5299fa220abf14815ba655467edc66332fca
Hossein Yousefi <yousefi@google.com>
2025-08-12 16:21:17 +0200

    Fix jnigen examples

--- pkgs/jnigen/example/in_app_java/lib/main.dart
+++ pkgs/jnigen/example/in_app_java/lib/main.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:jni/jni.dart';
 
@@ -9,12 +11,12 @@ import 'package:jni/jni.dart';
 // structure.
 import 'android_utils.dart';
 
-JObject activity = JObject.fromReference(Jni.getCurrentActivity());
-JObject context = JObject.fromReference(Jni.getCachedApplicationContext());
+JObject context =
+    Jni.androidApplicationContext(PlatformDispatcher.instance.engineId!);
 
 final hashmap = HashMap.new$2(K: JString.type, V: JString.type);
 
-final emojiCompat = EmojiCompat.get()!;
+final emojiCompat = EmojiCompat.get();
 
 extension IntX on int {
   JString toJString() {
@@ -26,9 +28,11 @@ const sunglassEmoji = "😎";
 
 /// Display device model number and the number of times this was called
 /// as Toast.
-void showToast() {
-  final toastCount =
-      hashmap.getOrDefault("toastCount".toJString(), 0.toJString());
+void showToast(JObject activity) {
+  final toastCount = hashmap.getOrDefault(
+    "toastCount".toJString(),
+    0.toJString(),
+  );
   final newToastCount = (int.parse(toastCount!.toDartString()) + 1).toJString();
   hashmap.put("toastCount".toJString(), newToastCount);
   final emoji = emojiCompat.hasEmojiGlyph(sunglassEmoji.toJString())
@@ -51,36 +55,54 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final Stream<JObject?> activityStream;
+
+  @override
+  void initState() {
+    activityStream =
+        Jni.androidActivities(PlatformDispatcher.instance.engineId!);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: const Text('Show Device Model'),
-              onPressed: () => showToast(),
+    return StreamBuilder(
+        stream: activityStream,
+        builder: (context, asyncSnapshot) {
+          if (!asyncSnapshot.hasData || asyncSnapshot.data == null) {
+            return Container();
+          }
+          final activity = asyncSnapshot.data!;
+          return Scaffold(
+            appBar: AppBar(title: Text(widget.title)),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: const Text('Show Device Model'),
+                    onPressed: () => showToast(activity),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
