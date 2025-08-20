62a8d8b1e3005580209ff476ed94b18a0f481957
Liam Appelbe <liama@google.com>
2025-08-20 15:08:59 +1000

    [swift2objc] More built in declarations

---
+++ pkgs/swift2objc/lib/src/parser/parsers/declaration_parsers/parse_built_in_declaration.dart
@@ -0,0 +1,17 @@
+// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import '../../../ast/declarations/built_in/built_in_declaration.dart';
+import '../../_core/json.dart';
+import '../../_core/parsed_symbolgraph.dart';
+import '../../_core/token_list.dart';
+import '../../_core/utils.dart';
+import '../parse_type.dart';
+
+BuiltInDeclaration? tryParseBuiltInDeclaration(
+    Json symbolJson) {
+  final id = parseSymbolId(symbolJson);
+  if (!id.startsWith('c:objc(cs)')) return null;
+  return BuiltInDeclaration(id: id, name: parseSymbolName(symbolJson));
+}
