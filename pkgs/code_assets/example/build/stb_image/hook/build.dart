b287b0b8014b3001df623196eb444f96b9cbd511
Daco Harkes <dacoharkes@google.com>
2025-08-25 13:31:14 +0200

    [code_assets] Add `stb_image` example

---
+++ pkgs/code_assets/example/build/stb_image/hook/build.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:code_assets/code_assets.dart';
+import 'package:hooks/hooks.dart';
+import 'package:logging/logging.dart';
+import 'package:native_toolchain_c/native_toolchain_c.dart';
+
+void main(List<String> args) async {
+  await build(args, (input, output) async {
+    if (input.config.buildCodeAssets) {
+      final cbuilder = CBuilder.library(
+        name: 'stb_image',
+        assetName: 'src/third_party/stb_image.g.dart',
+        sources: ['third_party/stb_image.c'],
+      );
+      await cbuilder.run(
+        input: input,
+        output: output,
+        logger: Logger('')
+          ..level = Level.ALL
+          ..onRecord.listen((record) => print(record.message)),
+      );
+    }
+  });
+}
