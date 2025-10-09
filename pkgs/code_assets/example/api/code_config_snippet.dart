64e0f53f0934ecade81af972b25939366a2a2d4a
Daco Harkes <dacoharkes@google.com>
2025-10-09 16:21:48 +0200

    [hooks] [code_assets] Library dartdoc comments

---
+++ pkgs/code_assets/example/api/code_config_snippet.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2025, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// ignore_for_file: unused_local_variable
+
+// snippet-start
+import 'package:code_assets/code_assets.dart';
+import 'package:hooks/hooks.dart';
+
+void main(List<String> args) async {
+  await build(args, (input, output) async {
+    if (input.config.buildCodeAssets) {
+      final codeConfig = input.config.code;
+      final targetOS = codeConfig.targetOS;
+      final targetArchitecture = codeConfig.targetArchitecture;
+
+      // Add some code assets.
+    }
+  });
+}
+
+// snippet-end
