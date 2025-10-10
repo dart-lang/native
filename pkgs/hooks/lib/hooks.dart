6fe224fda03feb5931ac0a98ce8853c709c2dfac
Daco Harkes <dacoharkes@google.com>
2025-10-10 17:40:59 +0200

    [hooks] [code_assets] Library dartdoc comments (#2687)

--- pkgs/hooks/lib/hooks.dart
+++ pkgs/hooks/lib/hooks.dart
@@ -4,10 +4,46 @@
 
 /// @docImport 'src/api/build_and_link.dart';
 
-/// A library that contains the protocol for implementing hooks.
+/// This package provides the API for hooks in Dart. Hooks are Dart scripts
+/// placed in the `hook/` directory of a Dart package, designed to automate
+/// tasks for a Dart package.
 ///
-/// The main entrypoint for build hooks (`hook/build.dart`) is [build]. The main
-/// entrypoint for link hooks (`hook/link.dart`) is [link].
+/// Currently, the main supported hook is the build hook (`hook/build.dart`).
+/// The build hook is executed during a Dart build and enables you to bundle
+/// assets with a Dart package. The main entrypoint for build hooks is [build].
+///
+/// The second hook available in this API is the link hook  (`hook/link.dart`).
+/// The main entrypoint for link hooks is [link].
+///
+/// Hooks can for example be used to bundle native code with a Dart package:
+///
+/// <!-- file://./../../code_assets/example/sqlite/hook/build.dart -->
+/// ```dart
+/// import 'package:code_assets/code_assets.dart';
+/// import 'package:hooks/hooks.dart';
+/// import 'package:native_toolchain_c/native_toolchain_c.dart';
+///
+/// void main(List<String> args) async {
+///   await build(args, (input, output) async {
+///     if (input.config.buildCodeAssets) {
+///       final builder = CBuilder.library(
+///         name: 'sqlite3',
+///         assetName: 'src/third_party/sqlite3.g.dart',
+///         sources: ['third_party/sqlite/sqlite3.c'],
+///         defines: {
+///           if (input.config.code.targetOS == OS.windows)
+///             // Ensure symbols are exported in dll.
+///             'SQLITE_API': '__declspec(dllexport)',
+///         },
+///       );
+///       await builder.run(input: input, output: output);
+///     }
+///   });
+/// }
+/// ```
+///
+/// For more information see
+/// [dart.dev/tools/hooks](https://dart.dev/tools/hooks).
 library;
 
 export 'src/api/build_and_link.dart' show build, link;
