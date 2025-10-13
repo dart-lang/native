0d02e8979c15e7fb90fe0ea34818f630dd8b5b3a
Michael Goderbauer <goderbauer@google.com>
2025-10-13 10:33:38 +0200

    fix jnigen doc

--- pkgs/jnigen/lib/jnigen.dart
+++ pkgs/jnigen/lib/jnigen.dart
@@ -3,12 +3,15 @@
 // BSD-style license that can be found in the LICENSE file.
 
 /// This library exports a high level programmatic API to JNIgen, the entry
-/// point of which is the [runJniGenTask] function, which takes run
-/// configuration as a [JniGenTask].
+/// point of which is the [generateJniBindings] function, which a [Config]
+/// object as a run configuration.
 /// {@category Java Differences}
 /// {@category Lifecycle}
 /// {@category Threading}
 /// {@category Interface Implementation}
+///
+/// @docImport 'src/config/config.dart';
+/// @docImport 'src/generate_bindings.dart';
 library;
 
 export 'src/config/config.dart';
