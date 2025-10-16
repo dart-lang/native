752aebb6c3c693d3d5045f378439b7420831153e
Liam Appelbe <liama@google.com>
2025-10-16 14:45:20 +1100

    readme etc

--- pkgs/ffigen/example/objective_c/generate_code.dart
+++ pkgs/ffigen/example/objective_c/generate_code.dart
@@ -6,15 +6,29 @@ import 'package:ffigen/ffigen.dart';
 
 final config = FfiGenerator(
   headers: Headers(
+    // The entryPoints are the files that FFIgen should scan to find the APIs
+    // you want to generate bindings for. You can use the macSdkPath or
+    // iosSdkPath getters to find the Apple SDKs.
     entryPoints: [
       Uri.file(
         '$macSdkPath/System/Library/Frameworks/AVFAudio.framework/Headers/AVAudioPlayer.h',
       ),
     ],
   ),
-  objectiveC: ObjectiveC(interfaces: Interfaces.includeSet({'AVAudioPlayer'})),
+
+  // To tell FFIgen to generate Objective-C bindings, rather than C bindings,
+  // set the objectiveC field to a non-null value.
+  objectiveC: ObjectiveC(
+    // The interfaces field is used to tell FFIgen which interfaces to generate
+    // bindings for. There's also a protocols and a categories field.
+    interfaces: Interfaces.includeSet({'AVAudioPlayer'}),
+  ),
+
   output: Output(
+    // The Dart file where the bindings will be generated.
     dartFile: Uri.file('avf_audio_bindings.dart'),
+
+    // Preamble text to put at the top of the generated file.
     preamble: '''
 // Copyright (c) 2024, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
