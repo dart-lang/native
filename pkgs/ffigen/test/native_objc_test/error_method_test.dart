857e5cbbb0a9dfdf915d55212cd57b30289abe94
Liam Appelbe <liamappelbe@gmail.com>
2025-11-06 15:52:01 +1100

    fix more tests

--- pkgs/ffigen/test/native_objc_test/error_method_test.dart
+++ pkgs/ffigen/test/native_objc_test/error_method_test.dart
@@ -18,16 +18,6 @@ import 'util.dart';
 void main() {
   group('error_method_test', () {
     setUpAll(() {
-      // TODO(https://github.com/dart-lang/native/issues/1068): Remove this.
-      DynamicLibrary.open(
-        path.join(
-          packagePathForTests,
-          '..',
-          'objective_c',
-          'test',
-          'objective_c.dylib',
-        ),
-      );
       final dylib = File(
         path.join(
           packagePathForTests,
