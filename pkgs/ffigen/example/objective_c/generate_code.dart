7ab0d2ab2ee2a9c90dddc3bdefeff669f4d00564
Michael Goderbauer <goderbauer@google.com>
2025-11-06 09:52:00 +0100

    add bug

--- pkgs/ffigen/example/objective_c/generate_code.dart
+++ pkgs/ffigen/example/objective_c/generate_code.dart
@@ -35,6 +35,7 @@ final config = FfiGenerator(
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// TODO(https://github.com/dart-lang/native/issues/2748): Remove this ignore.
 // ignore_for_file: unused_import
 ''',
   ),
