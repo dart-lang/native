28413ee124a2a8771b4f2b2079c508573d82ef72
Daco Harkes <dacoharkes@google.com>
2025-11-21 15:11:23 +0100

    more lints

--- pkgs/record_use/test_data/drop_dylib_recording/lib/src/drop_dylib_recording.dart
+++ pkgs/record_use/test_data/drop_dylib_recording/lib/src/drop_dylib_recording.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+// ignore_for_file: experimental_member_use
+
 import 'package:meta/meta.dart';
 
 import 'drop_dylib_recording_bindings.dart' as bindings;
