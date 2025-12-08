84ae535c840e186188ceca38af723cb7314ed597
Hossein Yousefi <yousefi@google.com>
2025-12-08 20:11:47 +0100

    Regenerate ffigen bindings

--- pkgs/jni/tool/wrapper_generators/generate_c_extensions.dart
+++ pkgs/jni/tool/wrapper_generators/generate_c_extensions.dart
@@ -294,6 +294,8 @@ String? getWrapperFunc(CompoundMember field) {
 void writeGlobalJniEnvWrapper(Library library) {
   final jniEnvType = findCompound(library, envType);
   final fieldDecls = jniEnvType.members
+      // ignore ones that take va_list
+      .where((member) => !member.name.endsWith('V'))
       .map((member) => getFunctionFieldDecl(member, isField: true))
       .join('\n');
   final varArgsFunctions = jniEnvType.members
@@ -316,6 +318,10 @@ typedef struct $wrapperName {
   final functionWrappers = StringBuffer();
   final structInst = StringBuffer('$wrapperName globalJniEnv = {\n');
   for (final member in jniEnvType.members) {
+    if (member.name.endsWith('V')) {
+      // Ignore va_list functions.
+      continue;
+    }
     final wrapper = getWrapperFunc(member);
     if (wrapper == null) {
       structInst.writeln('.${member.name} = NULL,');
