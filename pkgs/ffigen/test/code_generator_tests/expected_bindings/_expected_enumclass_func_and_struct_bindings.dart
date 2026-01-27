8d3dc0a10767f8273ebfb4c1400d6b2e7ab896e1
Liam Appelbe <liamappelbe@gmail.com>
2026-01-27 16:13:39 +1100

    regen

--- pkgs/ffigen/test/code_generator_tests/expected_bindings/_expected_enumclass_func_and_struct_bindings.dart
+++ pkgs/ffigen/test/code_generator_tests/expected_bindings/_expected_enumclass_func_and_struct_bindings.dart
@@ -21,6 +21,17 @@ class Bindings {
     ffi.Pointer<T> Function<T extends ffi.NativeType>(String symbolName) lookup,
   ) : _lookup = lookup;
 
+  void funcWithBothEnums(Enum1 value1, int value2) {
+    return _funcWithBothEnums(value1.value, value2);
+  }
+
+  late final _funcWithBothEnumsPtr =
+      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Int, ffi.Int)>>(
+        'funcWithBothEnums',
+      );
+  late final _funcWithBothEnums = _funcWithBothEnumsPtr
+      .asFunction<void Function(int, int)>();
+
   Enum1 funcWithEnum1(Enum1 value) {
     return Enum1.fromValue(_funcWithEnum1(value.value));
   }
@@ -36,17 +47,6 @@ class Bindings {
   late final _funcWithEnum2Ptr =
       _lookup<ffi.NativeFunction<ffi.Int Function(ffi.Int)>>('funcWithEnum2');
   late final _funcWithEnum2 = _funcWithEnum2Ptr.asFunction<int Function(int)>();
-
-  void funcWithBothEnums(Enum1 value1, int value2) {
-    return _funcWithBothEnums(value1.value, value2);
-  }
-
-  late final _funcWithBothEnumsPtr =
-      _lookup<ffi.NativeFunction<ffi.Void Function(ffi.Int, ffi.Int)>>(
-        'funcWithBothEnums',
-      );
-  late final _funcWithBothEnums = _funcWithBothEnumsPtr
-      .asFunction<void Function(int, int)>();
 
   StructWithEnums funcWithStruct(StructWithEnums value) {
     return _funcWithStruct(value);
