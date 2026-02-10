3c579771d3a95d959ccf145cfcf9edc13e4deed4
Daco Harkes <dacoharkes@google.com>
2026-02-10 16:10:49 +0100

    [record_use] Require `package:` URIs in schema

---
+++ pkgs/record_use/test/json_schema/uri_pattern_test.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:json_schema/json_schema.dart';
+import 'package:native_test_helpers/native_test_helpers.dart';
+import 'package:test/test.dart';
+
+import '../test_data.dart';
+
+void main() {
+  final schemaUri = packageUri.resolve('doc/schema/record_use.schema.json');
+  final schemaJson =
+      jsonDecode(File.fromUri(schemaUri).readAsStringSync())
+          as Map<String, Object?>;
+  final schema = JsonSchema.create(schemaJson);
+
+  group('Identifier.uri pattern', () {
+    test('JSON schema validation succeeds for package URI', () {
+      final json = recordedUses.toJson();
+      final result = schema.validate(json);
+      expect(result.isValid, isTrue);
+    });
+
+    test('JSON schema validation fails for non-package URI', () {
+      final json = recordedUses.toJson();
+      // Modify the first recording's identifier URI to be invalid.
+      final recordings = json['recordings'] as List;
+      final recording = recordings[0] as Map;
+      final identifier = recording['identifier'] as Map;
+      identifier['uri'] = 'dart:core'; // Should start with package:
+
+      final result = schema.validate(json);
+      expect(result.isValid, isFalse);
+      expect(
+        result.errors.any((e) => e.message.contains('pattern')),
+        isTrue,
+      );
+    });
+  });
+}
+
+Uri packageUri = findPackageRoot('record_use');
