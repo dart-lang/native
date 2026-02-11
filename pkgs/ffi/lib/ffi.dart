55ce021fb426d48c71899ef7fdbdc677e8a80c9f
Daco Harkes <dacoharkes@google.com>
2026-02-11 14:21:27 +0100

    [ffi] Export leaked types

--- pkgs/ffi/lib/ffi.dart
+++ pkgs/ffi/lib/ffi.dart
@@ -2,7 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/allocation.dart' show calloc, malloc;
+export 'src/allocation.dart'
+    show CallocAllocator, MallocAllocator, calloc, malloc;
 export 'src/arena.dart';
 export 'src/utf16.dart';
 export 'src/utf8.dart';
