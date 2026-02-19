ad7a97b0157154dc445f4a63711f745ffd696fe5
Gurleen-kansary <Gurleen-kansray@users.noreply.github.com>
2026-02-19 22:11:29 +0530

    Move util.c to objective_c_helper, update imports (#2272)

--- pkgs/objective_c/test/ns_input_stream_test.dart
+++ pkgs/objective_c/test/ns_input_stream_test.dart
@@ -16,8 +16,7 @@ import 'dart:typed_data';
 import 'package:ffi/ffi.dart';
 import 'package:objective_c/objective_c.dart';
 import 'package:test/test.dart';
-
-import 'util.dart';
+import 'package:objective_c_helper/src/util.dart';
 
 Future<(int, Uint8List, bool, NSStreamStatus, NSError?)> read(
   NSInputStream stream,
