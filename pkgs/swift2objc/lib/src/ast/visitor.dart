4613ca2e3cebc335a87e785366f6cbdb9a0149e0
Hassnaa Mohamed <mhassna727@gmail.com>
2026-03-03 01:47:37 +0200

    [swift2objc] feat:support swift tuples(return types) (#3158)

--- pkgs/swift2objc/lib/src/ast/visitor.dart
+++ pkgs/swift2objc/lib/src/ast/visitor.dart
@@ -112,6 +112,7 @@ abstract class Visitation {
   void visitTupleType(TupleType node) => visitReferredType(node);
   void visitExtensionDeclaration(ExtensionDeclaration node) =>
       visitDeclaration(node);
+  void visitTupleType(TupleType node) => visitReferredType(node);
 
   /// Default behavior for all visit methods.
   void visitAstNode(AstNode node) => node.visitChildren(visitor);
