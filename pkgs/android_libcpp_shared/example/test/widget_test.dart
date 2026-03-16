d5d3ba049e04c7e14e854fd5fc8fa3991763a0c1
zeyus <zeyus@zeyus.com>
2026-03-16 15:40:09 +0100

    Dart tool fixes.

--- pkgs/android_libcpp_shared/example/test/widget_test.dart
+++ pkgs/android_libcpp_shared/example/test/widget_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
