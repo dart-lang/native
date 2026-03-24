179d4a176c5d938fbb53de42a370e16078c20f38
Liam Appelbe <liama@google.com>
2026-03-24 14:04:17 +1100

    clean up

--- pkgs/jni_flutter/lib/jni_flutter.dart
+++ pkgs/jni_flutter/lib/jni_flutter.dart
@@ -7,7 +7,7 @@ import 'src/generated_plugin.dart';
 
 export 'src/generated_plugin.dart' show JniPlugin;
 
-extension JniFlutter on Jni {
+abstract final class JniFlutter {
   /// Retrieves the global Android `ApplicationContext` associated with a
   /// Flutter engine.
   ///
@@ -37,6 +37,42 @@ extension JniFlutter on Jni {
   ///
   /// Do not store the returned `JObject` in a field or local variable that
   /// persists across an `await`.
+  ///
+  /// ---
+  ///
+  /// ### Correct Usage (Synchronous, "Get-and-Use"):
+  ///
+  /// ```dart
+  /// void safeCall() {
+  ///   // This is safe because the `Activity` is retrieved and used
+  ///   // in a single, unbroken, synchronous block.
+  ///   final activity = Jni.androidActivity(engineId);
+  ///   if (activity != null) {
+  ///     someGeneratedApi.doSomething(activity);
+  ///     activity.release();
+  ///   }
+  /// }
+  /// ```
+  ///
+  /// ### **DANGEROUS** Usage (Asynchronous Gap):
+  ///
+  /// ```dart
+  /// Future<void> dangerousCall() async {
+  ///   // 1. Get the Activity (e.g., Activity "A")
+  ///   final activity = Jni.androidActivity(engineId);
+  ///
+  ///   // 2. An `await` occurs. The main thread is freed.
+  ///   //    While waiting, Android might destroy Activity "A" and create "B".
+  ///   await someOtherFuture();
+  ///
+  ///   // 3. CRASH: The code resumes, but `activity` is now a stale
+  ///   //    reference to the destroyed Activity "A".
+  ///   if (activity != null) {
+  ///     someGeneratedApi.doSomething(activity); // This will crash
+  ///     activity.release();
+  ///   }
+  /// }
+  /// ```
   static JObject? androidActivity(int engineId) {
     return JniPlugin.getActivity(engineId);
   }
---
+++ pkgs/jni_flutter/tool/generate_jni_bindings.dart
@@ -0,0 +1,34 @@
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:jnigen/jnigen.dart';
+
+Future<void> main() async {
+  const preamble = '''
+// Copyright (c) 2026, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// ignore_for_file: prefer_relative_imports''';
+
+  final packageRoot = Platform.script.resolve('..');
+  await generateJniBindings(
+    Config(
+      androidSdkConfig: AndroidSdkConfig(
+        addGradleDeps: true,
+        androidExample: packageRoot.resolve('example/').toFilePath(),
+      ),
+      outputConfig: OutputConfig(
+        dartConfig: DartCodeOutputConfig(
+          path: packageRoot.resolve('lib/src/plugin/generated_plugin.dart'),
+          structure: OutputStructure.singleFile,
+        ),
+      ),
+      classes: ['com.github.dart_lang.jni.JniPlugin'],
+      preamble: preamble,
+    ),
+  );
+}
