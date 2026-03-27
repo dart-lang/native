8f7cd5829cb205550193332e8779ac5b3b0f4ddf
Liam Appelbe <liama@google.com>
2026-03-27 14:26:54 +1100

    fix integration test

--- pkgs/jni_flutter/android_test_runner/integration_test/integration_test.dart
+++ pkgs/jni_flutter/android_test_runner/integration_test/integration_test.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter_test/flutter_test.dart';
@@ -18,6 +17,6 @@ void main() {
     final engineId = PlatformDispatcher.instance.engineId;
     expect(engineId, isNotNull);
     expect(androidActivity(engineId!), isNotNull);
-    expect(androidActivity(engineId!)!.toString(), contains('Activity'));
+    expect(androidActivity(engineId).toString(), contains('Activity'));
   });
 }
