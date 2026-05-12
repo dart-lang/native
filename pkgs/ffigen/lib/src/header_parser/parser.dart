b1f4a8007e903bf3c68a1da53334d25cedd59db8
Liam Appelbe <liama@google.com>
2026-05-12 10:35:59 +1000

    clean

--- pkgs/ffigen/lib/src/header_parser/parser.dart
+++ pkgs/ffigen/lib/src/header_parser/parser.dart
@@ -48,7 +48,7 @@ List<Binding> parseToBindings(Context context) {
   final config = context.config;
 
   Pointer<Pointer<Utf8>> clangCmdArgs = nullptr;
-  final compilerOpts = <String>[
+  final compilerOpts = overrideCompilerOpts(<String>[
     // Add compiler opt for comment parsing for clang based on config.
     if (config.output.commentType.length != CommentLength.none &&
         config.output.commentType.style == CommentStyle.any)
@@ -62,12 +62,11 @@ List<Binding> parseToBindings(Context context) {
 
     // Add the user options last so they can override any other options.
     ...context.compilerOpts,
-  ];
+  ]);
 
-  final finalCompilerOpts = overrideCompilerOpts(compilerOpts);
-  context.logger.fine('CompilerOpts used: $finalCompilerOpts');
-  clangCmdArgs = createDynamicStringArray(finalCompilerOpts);
-  final cmdLen = finalCompilerOpts.length;
+  context.logger.fine('CompilerOpts used: $compilerOpts');
+  clangCmdArgs = createDynamicStringArray(compilerOpts);
+  final cmdLen = compilerOpts.length;
 
   // Contains all bindings. A set ensures we never have duplicates.
   final bindings = <Binding>{};
