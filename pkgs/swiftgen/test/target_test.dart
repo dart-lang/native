dc9b3a3704ebc3cab1732478ed0441a87de59969
maranix <ramanverma4183@gmail.com>
2026-06-01 16:37:15 +0530

    test(swiftgen): rewrite target propagation tests

--- pkgs/swiftgen/test/target_test.dart
+++ pkgs/swiftgen/test/target_test.dart
@@ -1,141 +1,134 @@
-// Copyright (c) 2025, the Dart project authors. Please see the AUTHORS file
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:io';
 
 import 'package:logging/logging.dart';
-import 'package:path/path.dart' as p;
+import 'package:path/path.dart' as path;
 import 'package:swiftgen/swiftgen.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('SwiftGenerator passes target and sdk to Swift2ObjCGenerator', () async {
-    final tempDir = Directory.systemTemp.createTempSync('swiftgen_test');
-    try {
-      final outputFile = p.join(tempDir.path, 'output.dart');
-      final objectiveCFile = p.join(tempDir.path, 'output.m');
-      final swiftWrapperFile = p.join(tempDir.path, 'wrapper.swift');
-      final dummySwiftFile = p.join(tempDir.path, 'input.swift');
-      File(dummySwiftFile).writeAsStringSync('class Foo {}');
+  group('Target', () {
+    test('is passed to Swift2ObjCGenerator for SwiftFileInput', () async {
+      final tempDir = Directory.systemTemp.createTempSync('swiftgen_test');
+      addTearDown(() => tempDir.deleteSync(recursive: true));
 
-      final target = Target(
-        triple: 'invalid-triple',
-        sdk: Uri.file('/non/existent/sdk'),
-      );
+      final inputFile = path.join(tempDir.path, 'input.swift');
+      File(inputFile).writeAsStringSync('public class TestClass {}');
 
-      final generator = SwiftGenerator(
-        target: target,
-        inputs: [
-          SwiftFileInput(files: [Uri.file(dummySwiftFile)]),
-        ],
-        output: Output(
-          module: 'TestModule',
-          dartFile: Uri.file(outputFile),
-          objectiveCFile: Uri.file(objectiveCFile),
-          swiftWrapperFile: SwiftWrapperFile(path: Uri.file(swiftWrapperFile)),
-        ),
-        ffigen: const FfiGeneratorOptions(),
-      );
-
-      try {
-        await generator.generate(logger: Logger.root);
-        fail('Should have thrown an exception');
-      } catch (e) {
-        // The error should contain both the invalid triple and the invalid SDK
-        // because they are passed to swiftc.
-        expect(e.toString(), contains('invalid-triple'));
-        expect(e.toString(), contains('/non/existent/sdk'));
-      }
-    } finally {
-      tempDir.deleteSync(recursive: true);
-    }
-  });
-
-  test(
-    'SwiftGenerator passes target & sdk to Swift2ObjCGenerator for SwiftModuleInput',
-    () async {
-      final tempDir = Directory.systemTemp.createTempSync(
-        'swiftgen_test_module',
-      );
-      try {
-        final outputFile = p.join(tempDir.path, 'output.dart');
-        final objectiveCFile = p.join(tempDir.path, 'output.m');
-        final swiftWrapperFile = p.join(tempDir.path, 'wrapper.swift');
-
-        final target = Target(
-          triple: 'invalid-triple-module',
-          sdk: Uri.file('/non/existent/sdk/module'),
-        );
-
-        final generator = SwiftGenerator(
-          target: target,
-          inputs: [const SwiftModuleInput(module: 'SomeModule')],
-          output: Output(
-            module: 'TestModule',
-            dartFile: Uri.file(outputFile),
-            objectiveCFile: Uri.file(objectiveCFile),
-            swiftWrapperFile: SwiftWrapperFile(
-              path: Uri.file(swiftWrapperFile),
-            ),
-          ),
-          ffigen: const FfiGeneratorOptions(),
-        );
-
-        try {
-          await generator.generate(logger: Logger.root);
-          fail('Should have thrown an exception');
-        } catch (e) {
-          expect(e.toString(), contains('invalid-triple-module'));
-          expect(e.toString(), contains('/non/existent/sdk/module'));
-        }
-      } finally {
-        tempDir.deleteSync(recursive: true);
-      }
-    },
-  );
-
-  test(
-    'SwiftGenerator passes target and sdk to swiftc in _generateObjCFile',
-    () async {
-      final tempDir = Directory.systemTemp.createTempSync('swiftgen_test_objc');
-      try {
-        final outputFile = p.join(tempDir.path, 'output.dart');
-        final objectiveCFile = p.join(tempDir.path, 'output.m');
-        final dummySwiftFile = p.join(tempDir.path, 'input.swift');
-        // Needs to be @objc to be compatible without a wrapper
-        File(dummySwiftFile).writeAsStringSync(
-          'import Foundation\n@objc public class Foo: NSObject {}',
-        );
-
-        final target = Target(
-          triple: 'invalid-triple-objc',
-          sdk: Uri.file('/non/existent/sdk/objc'),
-        );
-
-        final generator = SwiftGenerator(
+      await expectTargetPassedToGeneratorCommand(
+        tempDir: tempDir,
+        executable: 'swiftc',
+        generateWrapper: true,
+        createGenerator: (target, output) => SwiftGenerator(
           target: target,
           inputs: [
-            ObjCCompatibleSwiftFileInput(files: [Uri.file(dummySwiftFile)]),
+            SwiftFileInput(files: [Uri.file(inputFile)]),
           ],
-          output: Output(
-            module: 'TestModule',
-            dartFile: Uri.file(outputFile),
-            objectiveCFile: Uri.file(objectiveCFile),
-          ),
+          output: output,
           ffigen: const FfiGeneratorOptions(),
-        );
+        ),
+      );
+    });
 
-        try {
-          await generator.generate(logger: Logger.root);
-          fail('Should have thrown an exception');
-        } catch (e) {
-          expect(e.toString(), contains('invalid-triple-objc'));
-          expect(e.toString(), contains('/non/existent/sdk/objc'));
-        }
-      } finally {
-        tempDir.deleteSync(recursive: true);
-      }
-    },
+    test('is passed to Swift2ObjCGenerator for SwiftModuleInput', () async {
+      final tempDir = Directory.systemTemp.createTempSync('swiftgen_test');
+      addTearDown(() => tempDir.deleteSync(recursive: true));
+
+      await expectTargetPassedToGeneratorCommand(
+        tempDir: tempDir,
+        executable: 'swift',
+        generateWrapper: true,
+        createGenerator: (target, output) => SwiftGenerator(
+          target: target,
+          inputs: [const SwiftModuleInput(module: 'TestModule')],
+          output: output,
+          ffigen: const FfiGeneratorOptions(),
+        ),
+      );
+    });
+
+    test('is passed to _generateObjCFile', () async {
+      final tempDir = Directory.systemTemp.createTempSync('swiftgen_test');
+      addTearDown(() => tempDir.deleteSync(recursive: true));
+
+      final inputFile = path.join(tempDir.path, 'input.swift');
+      File(inputFile).writeAsStringSync('''
+import Foundation
+
+@objc public class TestClass: NSObject {}
+''');
+
+      await expectTargetPassedToGeneratorCommand(
+        tempDir: tempDir,
+        executable: 'swiftc',
+        generateWrapper: false,
+        createGenerator: (target, output) => SwiftGenerator(
+          target: target,
+          inputs: [
+            ObjCCompatibleSwiftFileInput(files: [Uri.file(inputFile)]),
+          ],
+          output: output,
+          ffigen: const FfiGeneratorOptions(),
+        ),
+      );
+    });
+  });
+}
+
+Future<void> expectTargetPassedToGeneratorCommand({
+  required Directory tempDir,
+  required String executable,
+  required bool generateWrapper,
+  required SwiftGenerator Function(Target target, Output output)
+  createGenerator,
+}) async {
+  final outputFile = path.join(tempDir.path, 'output.dart');
+  final outputObjCFile = path.join(tempDir.path, 'output.m');
+  final wrapperFile = path.join(tempDir.path, 'wrapper.swift');
+  final sdkDir = path.join(tempDir.path, 'TestSDK.sdk');
+  final target = Target(
+    triple: 'arm64-apple-ios999.0',
+    sdk: Uri.directory(sdkDir),
+  );
+
+  Directory(sdkDir).createSync();
+
+  final output = Output(
+    swiftWrapperFile: generateWrapper
+        ? SwiftWrapperFile(path: Uri.file(wrapperFile))
+        : null,
+    module: 'TargetTest',
+    dartFile: Uri.file(outputFile),
+    objectiveCFile: Uri.file(outputObjCFile),
+  );
+
+  final generate = createGenerator(target, output).generate(
+    logger: Logger.detached('test')..level = Level.OFF,
+    tempDirectory: Uri.directory(tempDir.path),
+  );
+
+  final error = await generate.then<Object?>(
+    (_) => null,
+    onError: (Object error) => error,
+  );
+
+  expect(error, isA<ProcessException>());
+  final exception = error as ProcessException;
+  expect(exception.executable, executable);
+  expect(exception.arguments, containsPair('-target', target.triple));
+  expect(
+    exception.arguments,
+    containsPair('-sdk', path.absolute(target.sdk.toFilePath())),
   );
 }
+
+Matcher containsPair(String option, String value) =>
+    predicate<List<String>>((arguments) {
+      final index = arguments.indexOf(option);
+      return index >= 0 &&
+          index + 1 < arguments.length &&
+          arguments[index + 1] == value;
+    }, 'contains $option $value');
