2f6ba50e462ca2fed53d9c6ae24a31f996df9ceb
Hassnaa Mohamed <mhassna727@gmail.com>
2026-06-25 01:19:35 +0300

    Remove C++ configuration from YAML format

--- pkgs/ffigen/lib/src/config_provider/yaml_config.dart
+++ pkgs/ffigen/lib/src/config_provider/yaml_config.dart
@@ -73,10 +73,6 @@ final class YamlConfig {
   YamlDeclarationFilters get structDecl => _structDecl;
   late YamlDeclarationFilters _structDecl;
 
-  /// Declaration config for C++ classes. Null if `cpp` key is absent.
-  YamlDeclarationFilters? _cppClassDecl;
-  bool _hasCpp = false;
-
   /// Declaration config for Unions.
   YamlDeclarationFilters get unionDecl => _unionDecl;
   late YamlDeclarationFilters _unionDecl;
@@ -555,34 +551,6 @@ final class YamlConfig {
                   (node.value as Map)[strings.dependencyOnly]
                       as CompoundDependencies;
             },
-          ),
-        ),
-        HeterogeneousMapEntry(
-          key: strings.cpp,
-          valueConfigSpec: HeterogeneousMapConfigSpec(
-            result: (node) {
-              if (node.rawValue != null) {
-                _hasCpp = true;
-              }
-            },
-            entries: [
-              HeterogeneousMapEntry(
-                key: strings.cppClasses,
-                valueConfigSpec: HeterogeneousMapConfigSpec(
-                  entries: [
-                    ..._includeExcludeProperties(),
-                    ..._renameProperties(),
-                    ..._memberRenameProperties(),
-                  ],
-                  result: (node) {
-                    _cppClassDecl = declarationConfigExtractor(
-                      node.value as Map<dynamic, dynamic>,
-                      _excludeAllByDefault,
-                    );
-                  },
-                ),
-              ),
-            ],
           ),
         ),
         HeterogeneousMapEntry(
@@ -1298,15 +1266,6 @@ final class YamlConfig {
       // ignore: deprecated_member_use_from_same_package
       imported: structTypeMappings.values.toList(),
     ),
-    cpp: _hasCpp && _cppClassDecl != null
-        ? Cpp(
-            classes: CppClasses(
-              include: _cppClassDecl!.shouldInclude,
-              rename: _cppClassDecl!.rename,
-              renameMember: _cppClassDecl!.renameMember,
-            ),
-          )
-        : null,
     enums: Enums(
       include: _enumClassDecl.shouldInclude,
       rename: _enumClassDecl.rename,
--- pkgs/ffigen/test/native_cpp_test/util.dart
+++ pkgs/ffigen/test/native_cpp_test/util.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:ffigen/src/config_provider/config.dart';
 import 'package:ffigen/src/header_parser.dart' show parse;
 import 'package:path/path.dart' as p;
 
@@ -10,7 +11,28 @@ import '../test_utils.dart';
 void verifyBindings(String testName) {
   final thisDir = p.join(packagePathForTests, 'test', 'native_cpp_test');
   final configFile = p.join(thisDir, '${testName}_config.yaml');
-  final config = testConfigFromPath(configFile);
+  final yamlConfig = testConfigFromPath(configFile);
+  final config = FfiGenerator(
+    headers: yamlConfig.headers,
+    enums: yamlConfig.enums,
+    functions: yamlConfig.functions,
+    globals: yamlConfig.globals,
+    integers: yamlConfig.integers,
+    macros: yamlConfig.macros,
+    structs: yamlConfig.structs,
+    typedefs: yamlConfig.typedefs,
+    unions: yamlConfig.unions,
+    unnamedEnums: yamlConfig.unnamedEnums,
+    objectiveC: yamlConfig.objectiveC,
+    output: yamlConfig.output,
+    // ignore: deprecated_member_use_from_same_package
+    importedTypesByUsr: yamlConfig.importedTypesByUsr,
+    // ignore: deprecated_member_use_from_same_package
+    libraryImports: yamlConfig.libraryImports,
+    // ignore: deprecated_member_use_from_same_package
+    libclangDylib: yamlConfig.libclangDylib,
+    cpp: Cpp(classes: CppClasses.includeSet({'Animal'})),
+  );
   final context = testContext(config);
   final library = parse(context);
   final bindingsName = context.config.output.dartFile.pathSegments.last;
