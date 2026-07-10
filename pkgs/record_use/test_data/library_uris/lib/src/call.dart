b6063de70a50d5f597049d4443ac2005eefdc7c9
Daco Harkes <dacoharkes@google.com>
2026-07-10 13:26:49 +0200

    [record_use] Disable `public_member_api_docs` in `test_data` via `analysis_options.yaml`

--- pkgs/record_use/test_data/library_uris/lib/src/call.dart
+++ pkgs/record_use/test_data/library_uris/lib/src/call.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-// ignore_for_file: public_member_api_docs
-
 import 'package:library_uris_helper/library_uris_helper.dart';
 
 import 'definition.dart';
