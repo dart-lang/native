aca3a4615e2a00b459784ef4f22c80e7c141693a
Liam Appelbe <liama@google.com>
2026-08-18 09:26:13 +1000

    [ffigen] Regression test for #3548 (#3559)

---
+++ pkgs/ffigen/test/rename_tests/dependency_rename.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+#include "dependency_rename_dep.h"
+
+void some_func(struct DepStruct* s);
