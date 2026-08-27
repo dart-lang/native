2615415106b60c415fab2928b5faf04053d5e47c
Liam Appelbe <liama@google.com>
2026-08-27 13:19:30 +1000

    Fix analysis error

--- pkgs/ffigen/test/header_parser_tests/cpp_cyclic_class_test.dart
+++ pkgs/ffigen/test/header_parser_tests/cpp_cyclic_class_test.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:ffigen/ffigen.dart';
 import 'package:ffigen/src/code_generator.dart' as cg;
 import 'package:ffigen/src/header_parser.dart' as parser;
