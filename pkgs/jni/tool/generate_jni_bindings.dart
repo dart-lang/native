08f99bf7e6683a32496cc87fc374bd8bb2eb89e9
Liam Appelbe <liama@google.com>
2026-08-31 09:31:13 +1000

    Make renamer private

--- pkgs/jni/tool/generate_jni_bindings.dart
+++ pkgs/jni/tool/generate_jni_bindings.dart
@@ -21,8 +21,8 @@ const Map<String, String> _constructorAllowList = {
   'Short': 's',
 };
 
-base class Renamer extends Visitor {
-  Renamer() : super.base();
+base class _JniRenamer extends Visitor {
+  _JniRenamer() : super.base();
 
   ClassDecl? _currentClass;
 
@@ -91,7 +91,7 @@ Future<void> main() async {
       generateStubs: false,
     ),
     imports: SymbolImports(hide: classes),
-    visitors: [Renamer()],
+    visitors: [_JniRenamer()],
   );
   await generator.generate();
 }
