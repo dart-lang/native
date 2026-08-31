9772878c62c9f9692dcbd9812047b4aa33987678
Liam Appelbe <liama@google.com>
2026-08-31 14:35:10 +1000

    fix generation script error

--- pkgs/jni_flutter/android_test_runner/android/app/build.gradle.kts
+++ pkgs/jni_flutter/android_test_runner/android/app/build.gradle.kts
@@ -15,9 +15,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
+}
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
