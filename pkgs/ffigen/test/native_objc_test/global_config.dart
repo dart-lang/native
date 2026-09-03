ef6db8652b9a60cd61ab95f4194f1c35b7a39ef2
Liam Appelbe <liama@google.com>
2026-09-04 08:41:20 +1000

    [ffigen] Migrate test YAML configs (#3613)

---
+++ pkgs/ffigen/test/native_objc_test/global_config.dart
@@ -0,0 +1,31 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:ffigen/ffigen.dart';
+
+FfiGenerator getConfig([Uri? packageRoot]) {
+  packageRoot ??= Uri.directory(Directory.current.path);
+  final testDir = packageRoot.resolve('test/native_objc_test/');
+  return FfiGenerator(
+    output: Output(
+      dart: DartOutput(path: testDir.resolve('global_test_bindings.dart')),
+      style: const DynamicLibraryBindings(
+        wrapperName: 'GlobalTestObjCLibrary',
+        wrapperDocComment: 'Tests global variables',
+      ),
+    ),
+    input: Input(entryPoints: [testDir.resolve('global_test.h')]),
+    objectiveC: const ObjectiveC(),
+    visitors: [
+      Visitor(
+        global: (node) {
+          const include = {'globalString', 'globalObject', 'globalBlock'};
+          node.isIncluded = include.contains(node.name);
+        },
+      ),
+    ],
+  );
+}
