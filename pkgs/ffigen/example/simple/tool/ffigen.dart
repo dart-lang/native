2cb086a2fa9a3cb272114972c09a4d1284f5cd43
Liam Appelbe <liama@google.com>
2026-09-07 12:15:34 +1000

    [ffigen] Migrate remaining FFIgen YAML configs (#3614)

---
+++ pkgs/ffigen/example/simple/tool/ffigen.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:ffigen/ffigen.dart';
+
+FfiGenerator getConfig([Uri? packageRoot]) {
+  packageRoot ??= Platform.script.resolve('../');
+  return FfiGenerator(
+    output: Output(
+      dart: DartOutput(path: packageRoot.resolve('generated_bindings.dart')),
+      style: const DynamicLibraryBindings(
+        wrapperName: 'NativeLibrary',
+        wrapperDocComment: 'Bindings to `headers/example.h`.',
+      ),
+    ),
+    input: Input(entryPoints: [packageRoot.resolve('headers/example.h')]),
+    visitors: [Visitor(func: (node) => node.isIncluded = true)],
+  );
+}
+
+Future<void> main() async {
+  await getConfig().generate();
+}
--- pkgs/ffigen/test/large_integration_tests/large_test.dart
+++ pkgs/ffigen/test/large_integration_tests/large_test.dart
@@ -10,6 +10,7 @@ import 'package:logging/logging.dart';
 import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
+import '../../tool/generate_clang_bindings.dart' as generate_clang_bindings;
 import '../test_utils.dart';
 
 void main() {
@@ -249,8 +250,8 @@ void main() {
     });
 
     test('Libclang config test', () {
-      final config = testConfigFromPath(
-        path.join(packagePathForTests, 'tool', 'libclang_config.yaml'),
+      final config = generate_clang_bindings.getConfig(
+        Uri.file(path.join(packagePathForTests, '')),
       );
       final context = testContext(config);
       final library = parse(context);
