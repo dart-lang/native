b9fc7a26e860e10d93d63264cc88dbd6c10b08eb
Daco Harkes <dacoharkes@google.com>
2026-09-15 08:03:40 +0200

    [hooks] Fix Windows line endings in `bundle_prebuilt_assets` test

--- pkgs/hooks/example/build/bundle_prebuilt_assets/test/native_add_test.dart
+++ pkgs/hooks/example/build/bundle_prebuilt_assets/test/native_add_test.dart
@@ -64,8 +64,8 @@ void main() {
       final repoRoot = packageRoot.uri.resolve('../../../../../');
       final pubspecFile = File.fromUri(tempDir.uri.resolve('pubspec.yaml'));
       final pubspecContent = (await pubspecFile.readAsString())
-          .replaceFirst('resolution: workspace\n', '')
-          .replaceFirst('publish_to: none\n', '''
+          .replaceFirst('resolution: workspace', '')
+          .replaceFirst('publish_to: none', '''
 dependency_overrides:
   code_assets:
     path: ${repoRoot.resolve('pkgs/code_assets').toFilePath()}
@@ -74,8 +74,7 @@ dependency_overrides:
   native_toolchain_c:
     path: ${repoRoot.resolve('pkgs/native_toolchain_c').toFilePath()}
   ffigen:
-    path: ${repoRoot.resolve('pkgs/ffigen').toFilePath()}
-''');
+    path: ${repoRoot.resolve('pkgs/ffigen').toFilePath()}''');
       await pubspecFile.writeAsString(pubspecContent);
 
       final publishResult = await Process.run(Platform.resolvedExecutable, [
