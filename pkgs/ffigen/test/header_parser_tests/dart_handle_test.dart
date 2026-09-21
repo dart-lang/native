dbd64e4852b4de068c8c0b431fca4cd561215ec0
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-09-21 09:15:24 +0200

    [ffigen] Use `sdkPath!` from package:cli_util in dart_handle_test

--- pkgs/ffigen/test/header_parser_tests/dart_handle_test.dart
+++ pkgs/ffigen/test/header_parser_tests/dart_handle_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'package:cli_util/cli_util.dart';
 import 'package:ffigen/src/code_generator.dart';
-import 'package:ffigen/src/code_generator/utils.dart' show dartExecutable;
 import 'package:ffigen/src/header_parser.dart' as parser;
 import 'package:ffigen/src/strings.dart' as strings;
 import 'package:path/path.dart' as path;
@@ -22,7 +22,7 @@ void main() {
 ${strings.name}: 'NativeLibrary'
 ${strings.description}: 'Dart_Handle Test'
 ${strings.output}: 'unused'
-${strings.compilerOpts}: '-I${path.join(path.dirname(path.dirname(dartExecutable)), "include")}'
+${strings.compilerOpts}: '-I${path.join(sdkPath!, "include")}'
 
 ${strings.headers}:
   ${strings.entryPoints}:
